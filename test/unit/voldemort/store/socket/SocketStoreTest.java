f2469f5bd7efec0031d22f6b536935ee71bba19f
Jay Kreps <jay.kreps@gmail.com>
2009-02-12 22:33:33 +0000

    Fix type in socket test.

--- test/unit/voldemort/store/socket/SocketStoreTest.java
+++ test/unit/voldemort/store/socket/SocketStoreTest.java
@@ -73,9 +73,10 @@ public class SocketStoreTest extends ByteArrayStoreTest {
     public void testThreadOverload() throws Exception {
         final Store<byte[], byte[]> store = getStore();
         final AtomicInteger val = new AtomicInteger(0);
-        final CountDownLatch latch = new CountDownLatch(100);
+        int numOps = 100;
+        final CountDownLatch latch = new CountDownLatch(numOps);
         Executor exec = Executors.newCachedThreadPool();
-        for(int i = 0; i < 10; i++) {
+        for(int i = 0; i < numOps; i++) {
             exec.execute(new Runnable() {
 
                 public void run() {
