e7689cb10fb44773f31be7fbfa46cf65a9ab796c
Bhupesh Bansal <bbansal.usc@gmail.com>
2009-02-25 22:23:20 +0000

    upgrading BerkeleyDB Java Edition to 3.3.75

--- contrib/batch-indexer/src/java/test/voldemort/contrib/batchindexer/TestReadOnlyBatchIndexer.java
+++ contrib/batch-indexer/src/java/test/voldemort/contrib/batchindexer/TestReadOnlyBatchIndexer.java
@@ -23,6 +23,7 @@ import java.util.List;
 
 import junit.framework.TestCase;
 
+import org.apache.commons.io.FileDeleteStrategy;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.LongWritable;
@@ -56,10 +57,14 @@ public class TestReadOnlyBatchIndexer extends TestCase {
     @SuppressWarnings("unchecked")
     public void testCSVFileBatchIndexer() throws Exception {
 
-        ToolRunner.run(new Configuration(), new TextBatchIndexer(), null);
-
         // rename Files
         File dataDir = new File("contrib/batch-indexer/temp-output/text");
+        if(dataDir.exists()) {
+            FileDeleteStrategy.FORCE.delete(dataDir);
+        }
+
+        ToolRunner.run(new Configuration(), new TextBatchIndexer(), null);
+
         new File(dataDir, "users.index_0").renameTo(new File(dataDir, "users.index"));
         new File(dataDir, "users.data_0").renameTo(new File(dataDir, "users.data"));
 
