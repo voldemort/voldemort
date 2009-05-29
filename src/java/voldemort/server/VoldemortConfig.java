497e662f14dc64295b77942e299958868a737ac6
Elias Torres <elias@torrez.us>
2009-05-29 15:01:42 +0000

    Fixed typo in adminCoreThreads that was causing an IllegalArgumentException in ThreadPoolExecutor because coreThreads was greater than maxThreads.

--- src/java/voldemort/server/VoldemortConfig.java
+++ src/java/voldemort/server/VoldemortConfig.java
@@ -172,7 +172,7 @@ public class VoldemortConfig implements Serializable {
         this.coreThreads = props.getInt("core.threads", Math.max(1, maxThreads / 2));
 
         this.adminMaxThreads = props.getInt("admin.max.threads", 100);
-        this.adminCoreThreads = props.getInt("admin.core.threads", Math.max(1, maxThreads / 2));
+        this.adminCoreThreads = props.getInt("admin.core.threads", Math.max(1, adminMaxThreads / 2));
         this.adminStreamBufferSize = (int) props.getBytes("admin.streams.buffer.size",
                                                           10 * 1000 * 1000);
 
