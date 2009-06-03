652f4794bd49acbd6f069e1d3b3479646838c1a0
bbansal <bbansal.usc@gmail.com>
2009-06-02 17:06:13 -0700

    Fixed RedirectingStoreTest

--- test/unit/voldemort/store/rebalancing/RedirectingStoreTest.java
+++ test/unit/voldemort/store/rebalancing/RedirectingStoreTest.java
@@ -99,7 +99,8 @@ public class RedirectingStoreTest extends TestCase {
             ByteArray key = new ByteArray(ByteUtils.getBytes("" + i, "UTF-8"));
             byte[] value = ByteUtils.getBytes("value-" + i, "UTF-8");
 
-            Store<ByteArray, byte[]> store = server1.getStoreRepository().getLocalStore(storeName);
+            Store<ByteArray, byte[]> store = server1.getStoreRepository()
+                                                    .getStorageEngine(storeName);
             store.put(key,
                       Versioned.value(value,
                                       new VectorClock().incremented(0, System.currentTimeMillis())));
@@ -113,7 +114,7 @@ public class RedirectingStoreTest extends TestCase {
 
         RedirectingStore rebalancingStore = new RedirectingStore(0,
                                                                  server0.getStoreRepository()
-                                                                        .getLocalStore(storeName),
+                                                                        .getStorageEngine(storeName),
                                                                  metadata,
                                                                  new SocketPool(100,
                                                                                 100,
@@ -159,7 +160,8 @@ public class RedirectingStoreTest extends TestCase {
             ByteArray key = new ByteArray(ByteUtils.getBytes("" + i, "UTF-8"));
             byte[] value = ByteUtils.getBytes("value-" + i, "UTF-8");
 
-            Store<ByteArray, byte[]> store = server1.getStoreRepository().getLocalStore(storeName);
+            Store<ByteArray, byte[]> store = server1.getStoreRepository()
+                                                    .getStorageEngine(storeName);
             store.put(key,
                       Versioned.value(value,
                                       new VectorClock().incremented(0, System.currentTimeMillis())));
@@ -181,7 +183,7 @@ public class RedirectingStoreTest extends TestCase {
 
         RedirectingStore rebalancingStore = new RedirectingStore(0,
                                                                  server0.getStoreRepository()
-                                                                        .getLocalStore(storeName),
+                                                                        .getStorageEngine(storeName),
                                                                  metadata,
                                                                  new SocketPool(100,
                                                                                 100,
