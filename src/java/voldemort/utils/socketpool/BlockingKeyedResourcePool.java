71d497b3155942cae4e47faedb195f40ce34235d
Ismael Juma <ismael@juma.me.uk>
2009-07-11 17:40:08 +0100

    Replace resourcesQueueMap and resourceQueueMap and resourceSizeMap with resourcesMap.
    By encapsulating the values of both maps into a single object, it's easier to reason about the code and we reduce the number of map operations.
    
    Also change close to use queue.poll without queue.size. The latter is not required if we simply want to drain the queue.

--- src/java/voldemort/utils/socketpool/BlockingKeyedResourcePool.java
+++ src/java/voldemort/utils/socketpool/BlockingKeyedResourcePool.java
@@ -1,7 +1,9 @@
 package voldemort.utils.socketpool;
 
+import java.util.Queue;
 import java.util.Map.Entry;
 import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 import java.util.concurrent.TimeUnit;
@@ -20,21 +22,31 @@ import org.apache.log4j.Logger;
  */
 public class BlockingKeyedResourcePool<K, V> {
 
+    private static class Resources<V> {
+
+        final BlockingQueue<V> queue;
+        final AtomicInteger size = new AtomicInteger(0);
+
+        public Resources(int defaultPoolSize) {
+            // TODO I (ijuma) am not convinced that we need a fair queue. Also,
+            // LinkedBlockingQueue may be better
+            queue = new ArrayBlockingQueue<V>(defaultPoolSize, true);
+        }
+    }
+
     private static final Logger logger = Logger.getLogger(BlockingKeyedResourcePool.class.getName());
 
     private final PoolableObjectFactory<K, V> objectFactory;
     private final ResourcePoolConfig config;
 
-    private final ConcurrentMap<K, ArrayBlockingQueue<V>> resourceQueueMap;
-    private final ConcurrentMap<K, AtomicInteger> resourceSizeMap;
+    private final ConcurrentMap<K, Resources<V>> resourcesMap;
     private final AtomicBoolean isRunningState = new AtomicBoolean(true);
 
     public BlockingKeyedResourcePool(PoolableObjectFactory<K, V> objectFactory,
                                      ResourcePoolConfig config) {
         this.objectFactory = objectFactory;
         this.config = config;
-        this.resourceQueueMap = new ConcurrentHashMap<K, ArrayBlockingQueue<V>>();
-        this.resourceSizeMap = new ConcurrentHashMap<K, AtomicInteger>();
+        resourcesMap = new ConcurrentHashMap<K, Resources<V>>();
     }
 
     public V borrowResource(K key) throws Exception {
@@ -44,21 +56,29 @@ public class BlockingKeyedResourcePool<K, V> {
 
         long startTime = System.currentTimeMillis();
 
+        Resources<V> resources = resourcesMap.get(key);
         // check and create blocking queue if needed
-        if(!resourceQueueMap.containsKey(key)) {
-            resourceSizeMap.putIfAbsent(key, new AtomicInteger(0));
-            resourceQueueMap.putIfAbsent(key,
-                                         new ArrayBlockingQueue<V>(config.getDefaultPoolSize(),
-                                                                   true));
+        if(resources == null) {
+            resources = new Resources<V>(config.getDefaultPoolSize());
+            Resources<V> prev = resourcesMap.putIfAbsent(key, resources);
+
+            // another thread won the race, so we discard the resources created
+            // by this thread
+            if(prev != null)
+                resources = prev;
         }
 
         // get a valid resource
-        V resource = getResource(key, config.getBorrowTimeout(), config.getReturnTimeoutUnit());
+        V resource = getResource(key,
+                                 resources,
+                                 config.getBorrowTimeout(),
+                                 config.getReturnTimeoutUnit());
         while(!objectFactory.validate(key, resource)) {
             // destroy bad resource
-            cleanDestroyResource(key, resource);
+            cleanDestroyResource(key, resources, resource);
             // try get new resource with updated timeout.
             resource = getResource(key,
+                                   resources,
                                    computeNewTimeout(startTime,
                                                      config.getBorrowTimeout(),
                                                      config.getBorrowTimeoutUnit()),
@@ -86,7 +106,7 @@ public class BlockingKeyedResourcePool<K, V> {
         return Math.max(1, borrowTimeout - timeElapsed);
     }
 
-    private void cleanDestroyResource(K key, V resource) throws Exception {
+    private void cleanDestroyResource(K key, Resources<V> resources, V resource) throws Exception {
         try {
             objectFactory.destroy(key, resource);
         } catch(Exception e) {
@@ -94,19 +114,20 @@ public class BlockingKeyedResourcePool<K, V> {
             logger.error("Exception while destorying invalid resource:", e);
             resource = null;
         } finally {
-            resourceSizeMap.get(key).decrementAndGet();
+            resources.size.decrementAndGet();
         }
     }
 
-    private V getResource(K key, long timeout, TimeUnit timeunit) throws Exception {
-        if(resourceSizeMap.get(key).get() < config.getDefaultPoolSize()) {
+    private V getResource(K key, Resources<V> resources, long timeout, TimeUnit timeunit)
+            throws Exception {
+        if(resources.size.get() < config.getDefaultPoolSize()) {
             // pool can grow if needed
-            V resource = getResourceOrGrowPool(key);
+            V resource = getResourceOrGrowPool(key, resources);
             if(null != resource)
                 return resource;
         }
         // pool has reached max size, do blocking get()
-        return resourceQueueMap.get(key).poll(timeout, timeunit);
+        return resources.queue.poll(timeout, timeunit);
     }
 
     /**
@@ -117,16 +138,16 @@ public class BlockingKeyedResourcePool<K, V> {
      * @return
      * @throws Exception
      */
-    private V getResourceOrGrowPool(K key) throws Exception {
+    private V getResourceOrGrowPool(K key, Resources<V> resources) throws Exception {
         // try getting resource w/o blocking
-        V resource = resourceQueueMap.get(key).poll();
+        V resource = resources.queue.poll();
 
         if(null == resource) {
             // need to grow pool
-            if(resourceSizeMap.get(key).incrementAndGet() <= config.getDefaultPoolSize()) {
+            if(resources.size.incrementAndGet() <= config.getDefaultPoolSize()) {
                 return objectFactory.create(key);
             } else {
-                resourceSizeMap.get(key).decrementAndGet();
+                resources.size.decrementAndGet();
                 return null;
             }
         }
@@ -135,17 +156,18 @@ public class BlockingKeyedResourcePool<K, V> {
     }
 
     public void returnResource(K key, V resource) throws Exception {
-        if(resourceQueueMap.containsKey(key)) {
+        Resources<V> resources = resourcesMap.get(key);
+        if(resources != null) {
             resource = objectFactory.passivate(key, resource);
 
             if(isRunningState.get()) {
                 // Running state add resource back to queue
-                resourceQueueMap.get(key).offer(resource,
-                                                config.getReturnTimeout(),
-                                                config.getBorrowTimeoutUnit());
+                resources.queue.offer(resource,
+                                      config.getReturnTimeout(),
+                                      config.getBorrowTimeoutUnit());
             } else {
                 // clean the resource
-                cleanDestroyResource(key, resource);
+                cleanDestroyResource(key, resources, resource);
             }
         }
     }
@@ -153,14 +175,14 @@ public class BlockingKeyedResourcePool<K, V> {
     public void close() throws Exception {
         // change state to false and allow one thread.
         if(isRunningState.compareAndSet(true, false)) {
-            for(Entry<K, ArrayBlockingQueue<V>> entry: resourceQueueMap.entrySet()) {
+            for(Entry<K, Resources<V>> entry: resourcesMap.entrySet()) {
+                Queue<V> queue = entry.getValue().queue;
                 // destroy each resource in the queue
-                while(entry.getValue().size() > 0) {
-                    cleanDestroyResource(entry.getKey(), entry.getValue().poll());
+                V value;
+                while((value = queue.poll()) != null) {
+                    cleanDestroyResource(entry.getKey(), entry.getValue(), value);
                 }
-                // remove queue/size from hashmaps
-                resourceQueueMap.remove(entry.getKey());
-                resourceSizeMap.remove(entry.getKey());
+                resourcesMap.remove(entry.getKey());
             }
         }
     }
