d1fe06e26e3e435a6fae00c763f79ad15129a559
Jay Kreps <jay.kreps@gmail.com>
2009-07-27 20:41:59 -0700

    Remove hard client dependency on protocol buffers.

--- src/java/voldemort/client/protocol/RequestFormatFactory.java
+++ src/java/voldemort/client/protocol/RequestFormatFactory.java
@@ -16,9 +16,6 @@
 
 package voldemort.client.protocol;
 
-import java.util.EnumMap;
-
-import voldemort.VoldemortException;
 import voldemort.client.protocol.pb.ProtoBuffClientRequestFormat;
 import voldemort.client.protocol.vold.VoldemortNativeClientRequestFormat;
 
@@ -31,21 +28,17 @@ import voldemort.client.protocol.vold.VoldemortNativeClientRequestFormat;
  */
 public class RequestFormatFactory {
 
-    private EnumMap<RequestFormatType, RequestFormat> typeToInstance;
+    public RequestFormatFactory() {}
 
-    public RequestFormatFactory() {
-        this.typeToInstance = new EnumMap<RequestFormatType, RequestFormat>(RequestFormatType.class);
-        this.typeToInstance.put(RequestFormatType.VOLDEMORT_V1,
-                                new VoldemortNativeClientRequestFormat());
-        this.typeToInstance.put(RequestFormatType.PROTOCOL_BUFFERS,
-                                new ProtoBuffClientRequestFormat());
-    }
-
-    public RequestFormat getRequestFormat(RequestFormatType type) {
-        RequestFormat format = this.typeToInstance.get(type);
-        if(type == null)
-            throw new VoldemortException("Unknown wire format " + type);
-        return format;
+    public synchronized RequestFormat getRequestFormat(RequestFormatType type) {
+        switch(type) {
+            case VOLDEMORT_V1:
+                return new VoldemortNativeClientRequestFormat();
+            case PROTOCOL_BUFFERS:
+                return new ProtoBuffClientRequestFormat();
+            default:
+                throw new IllegalArgumentException("Unknown request format type: " + type);
+        }
     }
 
 }
