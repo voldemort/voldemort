9e0ba425e280f3e9cdc0e7850bd45f4047faf97c
Bhupesh Bansal <bbansal.usc@gmail.com>
2009-10-06 17:15:00 -0700

    Fixed BdbIterator to swallow DataBaseException.

--- src/java/voldemort/server/scheduler/DataCleanupJob.java
+++ src/java/voldemort/server/scheduler/DataCleanupJob.java
@@ -90,9 +90,19 @@ public class DataCleanupJob<K, V> implements Runnable {
         } catch(Exception e) {
             logger.error("Error in data cleanup job for store " + store.getName() + ": ", e);
         } finally {
+            try {
+                closeIterator(iterator);
+                this.cleanupPermits.release();
+            }
+        }
+    }
+
+    private void closeIterator(ClosableIterator<Pair<K, Versioned<V>>> iterator) {
+        try {
             if(iterator != null)
                 iterator.close();
-            this.cleanupPermits.release();
+        } catch(Exception e) {
+            logger.error("Error in closing iterator " + store.getName() + " ", e);
         }
     }
 
