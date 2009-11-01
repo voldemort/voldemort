b9a57b9b887c1618603fd3ec0c2278311c436e9f
Ismael Juma <ismael@juma.me.uk>
2009-11-01 10:17:40 +0000

    Change bdb.max.logfile.size from 1GB to 60MB.
    This reduces pauses caused by the cleaner thread while compressing the log files
    and it's a multiple of bdb.checkpoint.interval.bytes (20MB) not to cause
    additional checkpoints.
    
    See also See Issue 164: Change default bdb.max.logfile.size.

--- src/java/voldemort/server/VoldemortConfig.java
+++ src/java/voldemort/server/VoldemortConfig.java
@@ -158,7 +158,7 @@ public class VoldemortConfig implements Serializable {
         this.bdbFlushTransactions = props.getBoolean("bdb.flush.transactions", false);
         this.bdbDataDirectory = props.getString("bdb.data.directory", this.dataDirectory
                                                                       + File.separator + "bdb");
-        this.bdbMaxLogFileSize = props.getBytes("bdb.max.logfile.size", 1024 * 1024 * 1024);
+        this.bdbMaxLogFileSize = props.getBytes("bdb.max.logfile.size", 60 * 1024 * 1024);
         this.bdbBtreeFanout = props.getInt("bdb.btree.fanout", 512);
         this.bdbCheckpointBytes = props.getLong("bdb.checkpoint.interval.bytes", 20 * 1024 * 1024);
         this.bdbCheckpointMs = props.getLong("bdb.checkpoint.interval.ms", 30 * Time.MS_PER_SECOND);
@@ -401,7 +401,7 @@ public class VoldemortConfig implements Serializable {
 
     /**
      * The maximum size of a single .jdb log file in bytes. Given by
-     * "bdb.max.logfile.size" default: 1G
+     * "bdb.max.logfile.size" default: 60MB
      */
     public long getBdbMaxLogFileSize() {
         return this.bdbMaxLogFileSize;
