41fb4706af24a826fa3a4b5c20b182048b23a809
kirktrue <kirk@mustardgrain.com>
2009-11-04 08:15:40 -0800

    Addressed error handling (awkwardly) so the script doesn't blindly continue on errors.

--- contrib/ec2-testing/examples/remotetest/remotetest.sh
+++ contrib/ec2-testing/examples/remotetest/remotetest.sh
@@ -25,17 +25,70 @@ iterations=${10}
 
 rm -f $LOG_SERVERS_FILE $LOG_CLIENTS_FILE $COMMANDS_FILE
 
-# Create multiple instances in EC2.
-./contrib/ec2-testing/bin/voldemort-ec2instancecreator.sh --accessid $accessKeyId --secretkey $secretKey --ami $ami --instances $numClients --keypairid $keypairId > $HOSTS_CLIENTS 2>> $LOG_SERVERS_FILE
-./contrib/ec2-testing/bin/voldemort-ec2instancecreator.sh --accessid $accessKeyId --secretkey $secretKey --ami $ami --instances $numServers --keypairid $keypairId > $HOSTS_SERVERS 2>> $LOG_SERVERS_FILE
+# Create instances in EC2.
+if [ $numClients -gt 0 ]
+	then
+		./contrib/ec2-testing/bin/voldemort-ec2instancecreator.sh --accessid $accessKeyId --secretkey $secretKey --ami $ami --instances $numClients --keypairid $keypairId > $HOSTS_CLIENTS 2>> $LOG_SERVERS_FILE
+		exitCode=$?
 
+		if [ $exitCode -ne 0 ]
+			then
+				echo "Exit code from voldemort-ec2instancecreator.sh for clients: $exitCode, please see logs for more details"
+				exit 1
+		fi
+fi
+
+if [ $numServers -gt 0 ]
+	then
+		./contrib/ec2-testing/bin/voldemort-ec2instancecreator.sh --accessid $accessKeyId --secretkey $secretKey --ami $ami --instances $numServers --keypairid $keypairId > $HOSTS_SERVERS 2>> $LOG_SERVERS_FILE
+		exitCode=$?
+
+		if [ $exitCode -ne 0 ]
+			then
+				echo "Exit code from voldemort-ec2instancecreator.sh for servers: $exitCode, please see logs for more details"
+				exit 1
+		fi
+fi
+
+# Generate the cluster.xml file
 ./contrib/ec2-testing/bin/voldemort-clustergenerator.sh --hostnames $HOSTS_SERVERS --partitions $partitions > $configDir/config/cluster.xml
+exitCode=$?
+
+if [ $exitCode -ne 0 ]
+	then
+		echo "Exit code from voldemort-clustergenerator.sh: $exitCode, please see logs for more details"
+		exit 1
+fi
 
 # Deploy our files to the remote host.
 ./contrib/ec2-testing/bin/voldemort-deployer.sh --hostnames $HOSTS_SERVERS --source `pwd` --sshprivatekey $EC2_RSAKEYPAIR --parent "server" --logging debug 2>> $LOG_SERVERS_FILE
-./contrib/ec2-testing/bin/voldemort-deployer.sh --hostnames $HOSTS_SERVERS --source $configDir/* --sshprivatekey $EC2_RSAKEYPAIR --parent "testconfig" --logging debug 2>> $LOG_SERVERS_FILE
-./contrib/ec2-testing/bin/voldemort-deployer.sh --hostnames $HOSTS_CLIENTS --source `pwd` --sshprivatekey $EC2_RSAKEYPAIR --parent "client" --logging debug 2>> $LOG_SERVERS_FILE
+exitCode=$?
 
+if [ $exitCode -ne 0 ]
+	then
+		echo "Exit code from voldemort-deployer.sh for deploying servers: $exitCode, please see logs for more details"
+		exit 1
+fi
+
+./contrib/ec2-testing/bin/voldemort-deployer.sh --hostnames $HOSTS_SERVERS --source $configDir/* --sshprivatekey $EC2_RSAKEYPAIR --parent "testconfig" --logging debug 2>> $LOG_SERVERS_FILE
+exitCode=$?
+
+if [ $exitCode -ne 0 ]
+	then
+		echo "Exit code from voldemort-deployer.sh for deploying server configuration: $exitCode, please see logs for more details"
+		exit 1
+fi
+
+./contrib/ec2-testing/bin/voldemort-deployer.sh --hostnames $HOSTS_CLIENTS --source `pwd` --sshprivatekey $EC2_RSAKEYPAIR --parent "client" --logging debug 2>> $LOG_SERVERS_FILE
+exitCode=$?
+
+if [ $exitCode -ne 0 ]
+	then
+		echo "Exit code from voldemort-deployer.sh for deploying clients: $exitCode, please see logs for more details"
+		exit 1
+fi
+
+# We can't easily check the exit code as we run this in the background...
 ./contrib/ec2-testing/bin/voldemort-clusterstarter.sh --hostnames $HOSTS_SERVERS --sshprivatekey $EC2_RSAKEYPAIR --voldemortroot "server/voldemort" --voldemorthome "testconfig" --clusterxml $configDir/config/cluster.xml --logging debug 2>> $LOG_SERVERS_FILE &
 
 sleep 10
@@ -56,6 +109,14 @@ do
 done
 
 ./contrib/ec2-testing/bin/voldemort-clusterremotetest.sh --hostnames $HOSTS_CLIENTS --sshprivatekey $EC2_RSAKEYPAIR --commands $COMMANDS_FILE --logging debug 2>> $LOG_SERVERS_FILE > $LOG_CLIENTS_FILE
+exitCode=$?
+
+if [ $exitCode -ne 0 ]
+	then
+		echo "Exit code from voldemort-clusterremotetest.sh: $exitCode, please see logs for more details"
+		exit 1
+fi
+
 
 # Send SIGTERM
 pids=`ps xwww | grep voldemort.utils.app.VoldemortClusterStarterApp | grep -v "run-class.sh" | grep -v "grep" | awk '{print $1}'`
