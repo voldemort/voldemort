5aa35c88c0de399424b8e01aebce8008f9d7401d
kirktrue <kirk@mustardgrain.com>
2009-11-05 16:05:58 -0800

    Removed EC2 business so that it can be used outside of EC2 and so that it can be run an arbitrary number of times.

--- contrib/ec2-testing/examples/remotetest/remotetest.sh
+++ contrib/ec2-testing/examples/remotetest/remotetest.sh
@@ -1,57 +1,26 @@
 #!/bin/sh
 
-HOSTS_SERVERS=/tmp/hosts-servers
-HOSTS_CLIENTS=/tmp/hosts-clients
 LOG_SERVERS_FILE=/tmp/log-servers
 LOG_CLIENTS_FILE=/tmp/log-clients
 COMMANDS_FILE=/tmp/commands
 
-if [ $# -ne 10 ]
+if [ $# -ne 6 ]
 	then
-		echo "Usage: $0 <access key ID> <secret key> <ami> <key pair ID> <# clients> <# servers> <partitions per server> <config dir> <# requests> <# iterations>"
+		echo "Usage: $0 <client hosts file> <server hosts file> <partitions per server> <config dir> <# requests> <# iterations>"
 		exit 1
 fi
 
-accessKeyId=$1
-secretKey=$2
-ami=$3
-keypairId=$4
-numClients=$5
-numServers=$6
-partitions=$7
-configDir=$8
-numRequests=$9
-iterations=${10}
+hostsClients=$1
+hostsServers=$2
+partitions=$3
+configDir=$4
+numRequests=$5
+iterations=$6
 
 rm -f $LOG_SERVERS_FILE $LOG_CLIENTS_FILE $COMMANDS_FILE
 
-# Create instances in EC2.
-if [ $numClients -gt 0 ]
-	then
-		./contrib/ec2-testing/bin/voldemort-ec2instancecreator.sh --accessid $accessKeyId --secretkey $secretKey --ami $ami --instances $numClients --keypairid $keypairId > $HOSTS_CLIENTS 2>> $LOG_SERVERS_FILE
-		exitCode=$?
-
-		if [ $exitCode -ne 0 ]
-			then
-				echo "Exit code from voldemort-ec2instancecreator.sh for clients: $exitCode, please see logs for more details"
-				exit 1
-		fi
-fi
-
-if [ $numServers -gt 0 ]
-	then
-		./contrib/ec2-testing/bin/voldemort-ec2instancecreator.sh --accessid $accessKeyId --secretkey $secretKey --ami $ami --instances $numServers --keypairid $keypairId > $HOSTS_SERVERS 2>> $LOG_SERVERS_FILE
-		exitCode=$?
-
-		if [ $exitCode -ne 0 ]
-			then
-				echo "Exit code from voldemort-ec2instancecreator.sh for servers: $exitCode, please see logs for more details"
-				exit 1
-		fi
-fi
-
 # Generate the cluster.xml file
-./contrib/ec2-testing/bin/voldemort-clustergenerator.sh --hostnames $HOSTS_SERVERS --partitions $partitions > $configDir/config/cluster.xml
+./contrib/ec2-testing/bin/voldemort-clustergenerator.sh --hostnames $hostsServers --partitions $partitions > $configDir/config/cluster.xml
 exitCode=$?
 
 if [ $exitCode -ne 0 ]
@@ -61,7 +30,7 @@ if [ $exitCode -ne 0 ]
 fi
 
 # Deploy our files to the remote host.
-./contrib/ec2-testing/bin/voldemort-deployer.sh --hostnames $HOSTS_SERVERS --source `pwd` --sshprivatekey $EC2_RSAKEYPAIR --parent "server" --logging debug 2>> $LOG_SERVERS_FILE
+./contrib/ec2-testing/bin/voldemort-deployer.sh --hostnames $hostsServers --source `pwd` --sshprivatekey $EC2_RSAKEYPAIR --parent "server" --logging debug 2>> $LOG_SERVERS_FILE
 exitCode=$?
 
 if [ $exitCode -ne 0 ]
@@ -70,7 +39,7 @@ if [ $exitCode -ne 0 ]
 		exit 1
 fi
 
-./contrib/ec2-testing/bin/voldemort-deployer.sh --hostnames $HOSTS_SERVERS --source $configDir/* --sshprivatekey $EC2_RSAKEYPAIR --parent "testconfig" --logging debug 2>> $LOG_SERVERS_FILE
+./contrib/ec2-testing/bin/voldemort-deployer.sh --hostnames $hostsServers --source $configDir/* --sshprivatekey $EC2_RSAKEYPAIR --parent "testconfig" --logging debug 2>> $LOG_SERVERS_FILE
 exitCode=$?
 
 if [ $exitCode -ne 0 ]
@@ -79,7 +48,7 @@ if [ $exitCode -ne 0 ]
 		exit 1
 fi
 
-./contrib/ec2-testing/bin/voldemort-deployer.sh --hostnames $HOSTS_CLIENTS --source `pwd` --sshprivatekey $EC2_RSAKEYPAIR --parent "client" --logging debug 2>> $LOG_SERVERS_FILE
+./contrib/ec2-testing/bin/voldemort-deployer.sh --hostnames $hostsClients --source `pwd` --sshprivatekey $EC2_RSAKEYPAIR --parent "client" --logging debug 2>> $LOG_SERVERS_FILE
 exitCode=$?
 
 if [ $exitCode -ne 0 ]
@@ -89,16 +58,16 @@ if [ $exitCode -ne 0 ]
 fi
 
 # We can't easily check the exit code as we run this in the background...
-./contrib/ec2-testing/bin/voldemort-clusterstarter.sh --hostnames $HOSTS_SERVERS --sshprivatekey $EC2_RSAKEYPAIR --voldemortroot "server/voldemort" --voldemorthome "testconfig" --clusterxml $configDir/config/cluster.xml --logging debug 2>> $LOG_SERVERS_FILE &
+./contrib/ec2-testing/bin/voldemort-clusterstarter.sh --hostnames $hostsServers --sshprivatekey $EC2_RSAKEYPAIR --voldemortroot "server/voldemort" --voldemorthome "testconfig" --clusterxml $configDir/config/cluster.xml --logging debug 2>> $LOG_SERVERS_FILE &
 
 sleep 10
 
-bootstrapHost=`tail -n 1 $HOSTS_SERVERS | cut -d'=' -f2`
+bootstrapHost=`tail -n 1 $hostsServers | cut -d'=' -f2`
 
 counter=0
 sleep=30
 
-cat $HOSTS_CLIENTS |
+cat $hostsClients |
 while read line
 do
 	rampSeconds=$(($sleep * $counter))
@@ -108,7 +77,7 @@ do
 	echo "$externalHost=cd client/voldemort ; sleep $rampSeconds ; ./bin/voldemort-remote-test.sh -r -w -d --iterations $iterations --start-key-index $startKeyIndex tcp://${bootstrapHost}:6666 test $numRequests" >> $COMMANDS_FILE
 done
 
-./contrib/ec2-testing/bin/voldemort-clusterremotetest.sh --hostnames $HOSTS_CLIENTS --sshprivatekey $EC2_RSAKEYPAIR --commands $COMMANDS_FILE --logging debug 2>> $LOG_SERVERS_FILE > $LOG_CLIENTS_FILE
+./contrib/ec2-testing/bin/voldemort-clusterremotetest.sh --hostnames $hostsClients --sshprivatekey $EC2_RSAKEYPAIR --commands $COMMANDS_FILE --logging debug 2>> $LOG_SERVERS_FILE > $LOG_CLIENTS_FILE
 exitCode=$?
 
 if [ $exitCode -ne 0 ]
