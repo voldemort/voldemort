01cd4ff0cbc50309cbb017852404742819cbbff3
Tatu Saloranta <tsaloranta@gmail.com>
2009-12-04 20:57:16 -0800

    Added license information (and related)

--- src/main/org/h2/compress/ChunkEncoder.java
+++ src/main/org/h2/compress/ChunkEncoder.java
@@ -1,3 +1,13 @@
+/* Licensed under the Apache License, Version 2.0 (the "License"); you may not use this
+ * file except in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS
+ * OF ANY KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
 package org.h2.compress;
 
 /**
@@ -6,8 +16,10 @@ package org.h2.compress;
  * is only used if it actually reduces chunk size (including overhead
  * of additional header bytes)
  *<p>
- * Code based on H2 Java LZF implementation.
+ * Code adapted from H2 project (http://www.h2database.com) Java LZF implementation
+ * by Thomas (which itself was inspired by original C code by Marc A Lehmann)
  * 
+ * @author Thomas Mueller
  * @author Tatu Saloranta
  */
 public class ChunkEncoder
--- src/main/org/h2/compress/LZFEncoder.java
+++ src/main/org/h2/compress/LZFEncoder.java
@@ -1,3 +1,14 @@
+/* Licensed under the Apache License, Version 2.0 (the "License"); you may not use this
+ * file except in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS
+ * OF ANY KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+
 package org.h2.compress;
 
 import java.io.IOException;
@@ -7,8 +18,10 @@ import java.io.IOException;
  * calls {@link ChunkEncoder} to compress individual chunks and
  * combines resulting chunks into contiguous output byte array.
  *<p>
- * Code based on H2 Java LZF implementation.
+ * Code adapted from H2 project (http://www.h2database.com) Java LZF implementation
+ * by Thomas (which itself was inspired by original C code by Marc A Lehmann)
  * 
+ * @author Thomas Mueller
  * @author tsaloranta@gmail.com
  */
 public class LZFEncoder
