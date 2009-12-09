fd6fc21d89c26c18a16c54b7ffbbfe4c2a6d50d8
Alex Feinberg <alex@strlen.net>
2009-12-08 16:58:02 -0800

    Added a simple voldemort shell, supporting "getmetadata" and "fetch" for now.

---
+++ src/java/voldemort/utils/VoldemortAdminClientShell.java
@@ -0,0 +1,11 @@
+package voldemort.utils;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: afeinber
+ * Date: Dec 8, 2009
+ * Time: 2:11:44 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class VoldemortAdminClientShell {
+}
