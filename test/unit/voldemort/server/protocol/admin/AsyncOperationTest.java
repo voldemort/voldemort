9757b06e6c742acd0b1da7dc3e2ee97c1a3849d9
Alex Feinberg <alex@strlen.net>
2010-01-13 03:16:47 -0800

    Added a test for AsyncOperationRunner, specifically to test the potential concurrency issues.

--- test/unit/voldemort/server/protocol/admin/AsyncOperationTest.java
+++ test/unit/voldemort/server/protocol/admin/AsyncOperationTest.java
@@ -1,15 +1,69 @@
 package voldemort.server.protocol.admin;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 import junit.framework.TestCase;
+import voldemort.server.scheduler.SchedulerService;
+import voldemort.utils.SystemTime;
+import voldemort.utils.Time;
 
 /**
  * @author afeinberg
  */
 public class AsyncOperationTest extends TestCase {
+
+    public void testAsyncOperationRunner() throws Exception {
+        SchedulerService schedulerService = new SchedulerService(2, SystemTime.INSTANCE);
+        AsyncOperationRunner asyncOperationRunner = new AsyncOperationRunner(schedulerService, 10);
+
+        final AtomicBoolean completedOp0 = new AtomicBoolean(false);
+        final AtomicBoolean completedOp1 = new AtomicBoolean(false);
+
+        int opId0 = asyncOperationRunner.getUniqueRequestId();
+        asyncOperationRunner.submitOperation(opId0,
+                                             new AsyncOperation(opId0, "op0") {
+                                                 @Override
+                                                 public void operate() throws Exception {
+                                                     Thread.sleep(1000);
+                                                     completedOp0.set(true);
+                                                 }
+
+                                                 @Override
+                                                 public void stop() {
+
+                                                 }
+                                             });
+
+        int opId1 = asyncOperationRunner.getUniqueRequestId();
+        asyncOperationRunner.submitOperation(opId1,
+                                             new AsyncOperation(opId1, "op1") {
+                                                 @Override
+                                                 public void operate() throws Exception {
+                                                     completedOp1.set(true);
+                                                 }
+
+                                                 @Override
+                                                 public void stop() {
+
+                                                 }
+                                             });
+
+        List<Integer> opList = asyncOperationRunner.getAsyncOperationList(false);
+        assertFalse("doesn't list completed operations", opList.contains(1));
+        assertTrue("lists a pending operation", opList.contains(0));
+        opList = asyncOperationRunner.getAsyncOperationList(true);
+        assertTrue("lists all operations", opList.containsAll(Arrays.asList(0,1)));
+
+        Thread.sleep(1000);
+
+        assertTrue("operation 0 finished", completedOp0.get());
+        assertTrue("operation 1 finished", completedOp1.get());
+    }
 
     @SuppressWarnings("unchecked")
     public void testAsyncOperationRepository() {
