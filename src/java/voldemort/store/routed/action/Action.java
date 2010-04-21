8596138ab2f6c82aeaddfc8f64fc42d3d263f0cc
Kirk True <kirk@mustardgrain.com>
2010-04-21 09:35:38 -0700

    Comments

--- src/java/voldemort/store/routed/action/Action.java
+++ src/java/voldemort/store/routed/action/Action.java
@@ -17,8 +17,33 @@
 package voldemort.store.routed.action;
 
 import voldemort.store.routed.Pipeline;
+import voldemort.store.routed.Pipeline.Event;
+
+/**
+ * An Action is executed in response to the {@link Pipeline} receiving an
+ * {@link Event}. An Action is a discrete portion of logic that forms part of
+ * the overall process that executes a given operation. There’s no clear
+ * standard about how much or how little logic is performed in a given Action,
+ * but there are intuitive separations in the logic that form natural
+ * boundaries.
+ * 
+ * <p/>
+ * 
+ * Actions are mapped to events by the {@link Pipeline} via the
+ * {@link Pipeline#setEventActions(java.util.Map)} method.
+ * 
+ * @see Pipeline#setEventActions(java.util.Map)
+ */
 
 public interface Action {
+
+    /**
+     * Executes some portion of the overall logic in the routing pipeline.
+     * 
+     * @param pipeline {@link Pipeline} instance of which this action is a part
+     * @param eventData Data included as part of the event, or null if not
+     *        applicable
+     */
 
     public void execute(Pipeline pipeline, Object eventData);
 
