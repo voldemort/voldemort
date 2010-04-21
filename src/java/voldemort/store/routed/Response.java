b10f8a53fc9283e7b3b1655c74764d5804ea5276
Kirk True <kirk@mustardgrain.com>
2010-04-20 21:59:15 -0700

    Comments

--- src/java/voldemort/store/routed/Response.java
+++ src/java/voldemort/store/routed/Response.java
@@ -17,6 +17,49 @@
 package voldemort.store.routed;
 
 import voldemort.cluster.Node;
+import voldemort.cluster.failuredetector.FailureDetector;
+import voldemort.store.nonblockingstore.NonblockingStore;
+import voldemort.store.nonblockingstore.NonblockingStoreCallback;
+import voldemort.store.routed.Pipeline.Event;
+import voldemort.utils.ByteArray;
+
+/**
+ * Response represents a response from a call to a remote Voldemort node to
+ * perform some operation (get, put, etc.). It includes the {@link Node} and
+ * request time as these are needed by the {@link FailureDetector} that will be
+ * used by the user of the Response.
+ * 
+ * <p/>
+ * 
+ * A Response is usually used in conjunction with asynchronous requests as a
+ * sort of <i>callback</i> mechanism, though this isn't always the case. In the
+ * case where they are the result of asynchronous requests, the
+ * {@link NonblockingStore} will invoke the {@link NonblockingStoreCallback}
+ * instances's <code>requestComplete</code> method which will in turn package up
+ * the data in a Response object that is sent to the {@link Pipeline} via an
+ * {@link Event#RESPONSE_RECEIVED} event.
+ * 
+ * <p/>
+ * 
+ * Response instances are stored in the {@link PipelineData} to represent the
+ * responses to requests made during execution of the {@link Pipeline}.
+ * 
+ * <p/>
+ * 
+ * This class uses generics for the value to support the return types used by
+ * the different operations. Oftentimes the key type is simply {@link ByteArray}
+ * , but in the case of the "get all" operation, the key is actually an
+ * {@link Iterable}.
+ * 
+ * @param <K> Type for the key used in the request
+ * @param <V> Type for the value returned by the call
+ * 
+ * @see BasicResponseCallback
+ * @see NonblockingStore
+ * @see NonblockingStoreCallback
+ * @see Pipeline
+ * @see PipelineData
+ */
 
 public class Response<K, V> {
 
