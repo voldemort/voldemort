1f21e507ffd56dd419fd70e38c09fc23179131cb
Alex Feinberg <alex@strlen.net>
2010-09-13 20:50:21 -0700

    Fixed issue 286: ReadOnlyStorageEngine#get should use file modification lock

--- src/java/voldemort/store/readonly/ReadOnlyStorageEngine.java
+++ src/java/voldemort/store/readonly/ReadOnlyStorageEngine.java
@@ -375,15 +375,20 @@ public class ReadOnlyStorageEngine implements StorageEngine<ByteArray, byte[]> {
     public List<Versioned<byte[]>> get(ByteArray key) throws VoldemortException {
         StoreUtils.assertValidKey(key);
         byte[] keyMd5 = ByteUtils.md5(key.get());
-        int chunk = fileSet.getChunkForKey(keyMd5);
-        int location = searchStrategy.indexOf(fileSet.indexFileFor(chunk),
-                                              keyMd5,
-                                              fileSet.getIndexFileSize(chunk));
-        if(location >= 0) {
-            byte[] value = readValue(chunk, location);
-            return Collections.singletonList(Versioned.value(value));
-        } else {
-            return Collections.emptyList();
+        try {
+            fileModificationLock.readLock().lock();
+            int chunk = fileSet.getChunkForKey(keyMd5);
+            int location = searchStrategy.indexOf(fileSet.indexFileFor(chunk),
+                                                  keyMd5,
+                                                  fileSet.getIndexFileSize(chunk));
+            if(location >= 0) {
+                byte[] value = readValue(chunk, location);
+                return Collections.singletonList(Versioned.value(value));
+            } else {
+                return Collections.emptyList();
+            }
+        } finally {
+            fileModificationLock.readLock().unlock();
         }
     }
 
