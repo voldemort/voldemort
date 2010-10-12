a1b19bd412bc0418ee9e1050884db8a42df09cc6
Roshan Sumbaly <rsumbaly@gmail.com>
2010-10-11 17:36:36 -0700

    Removed RebalanceClusterBuilder

--- src/java/voldemort/client/protocol/admin/AdminClient.java
+++ src/java/voldemort/client/protocol/admin/AdminClient.java
@@ -899,9 +899,9 @@ public class AdminClient {
         while(System.currentTimeMillis() < waitUntil) {
             try {
                 AsyncOperationStatus status = getAsyncRequestStatus(nodeId, requestId);
-                if(logger.isInfoEnabled())
-                    logger.info("Status from node " + nodeId + " (" + status.getDescription()
-                                + ") - " + status.getStatus());
+                if(logger.isDebugEnabled())
+                    logger.debug("Status from node " + nodeId + " (" + status.getDescription()
+                                 + ") - " + status.getStatus());
                 description = status.getDescription();
                 if(status.hasException())
                     throw status.getException();
