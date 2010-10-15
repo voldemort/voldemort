09c85562422032ce798a8b0f3e3f2b390a41e47b
Alex Feinberg <alex@strlen.net>
2010-10-15 14:00:26 -0700

    Clean up SlopPusherJob

--- src/java/voldemort/server/scheduler/SlopPusherJob.java
+++ src/java/voldemort/server/scheduler/SlopPusherJob.java
@@ -73,7 +73,7 @@ public class SlopPusherJob implements Runnable {
     public void run() {
         logger.debug("Pushing slop...");
 
-        int numNodes = storeRepo.getAllNodeStores().size();
+        int numNodes = cluster.getNumberOfNodes();
         Map<Integer, Long> attemptedByNode = Maps.newHashMapWithExpectedSize(numNodes);
         for(int i = 0; i < numNodes; i++)
             attemptedByNode.put(i, 0L);
