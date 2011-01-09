3d85f9c1dd846596200c9213637f6a2b7d3fc0eb
Roshan Sumbaly <rsumbaly@gmail.com>
2011-01-08 19:00:56 -0800

    Cleaning up integration grandfather test

--- test/integration/voldemort/store/grandfather/GrandfatherTest.java
+++ test/integration/voldemort/store/grandfather/GrandfatherTest.java
@@ -730,7 +730,6 @@ public class GrandfatherTest {
 
         Thread.sleep(10 * 1000);
 
-        System.out.println("*************** ALL SERVERS STARTED ***************");
         try {
             ClientConfig config = new ClientConfig().setBootstrapUrls("tcp://localhost:"
                                                                       + currentCluster.getNodeById(0)
@@ -813,8 +812,6 @@ public class GrandfatherTest {
                 for(int storeNo = 1; storeNo <= numStores; storeNo++) {
                     storeClients[storeNo - 1] = factory.getStoreClient("test" + storeNo);
                 }
-                System.out.println("=================");
-                System.out.println("ZONE " + zoneId);
                 for(int storeNo = 1; storeNo <= numStores; storeNo++) {
                     for(int i = 0; i < NUM_KEYS; i++) {
                         if(storeClients[storeNo - 1].get("key" + i, null) == null) {
@@ -832,14 +829,12 @@ public class GrandfatherTest {
                                                                                       + "_" + 2);
                         }
                     }
-                    System.out.println("------");
 
                 }
                 factory.close();
                 factory = null;
             }
         } catch(Exception e) {
-            System.out.println("=============ERRROR=============");
             e.printStackTrace();
         } finally {
             if(factory != null)
