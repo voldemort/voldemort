351e72963efb4f9ea03eb50e58ddb42fb6ce1373
Lei Gao <lgao@linkedin.com>
2011-06-28 01:05:11 -0700

    entropy returns distinct keys from each node

--- src/java/voldemort/utils/Entropy.java
+++ src/java/voldemort/utils/Entropy.java
@@ -195,7 +195,8 @@ public class Entropy {
                                                              false);
                                 for(long keyId = 0; keyId < numKeysPerNode && keys.hasNext(); keyId++) {
                                     ByteArray key = keys.next();
-                                    if(RebalanceUtils.getNodeIds(strategy.routeRequest(key.get()))
+                                    if(RebalanceUtils.getNodeIds(strategy.routeRequest(key.get())
+                                                                         .subList(0, 1))
                                                      .contains(node.getId())) {
                                         writer.write(key.length());
                                         writer.write(key.get());
@@ -211,7 +212,8 @@ public class Entropy {
                                                          false);
                             for(long keyId = 0; keyId < numKeys && keys.hasNext(); keyId++) {
                                 ByteArray key = keys.next();
-                                if(RebalanceUtils.getNodeIds(strategy.routeRequest(key.get()))
+                                if(RebalanceUtils.getNodeIds(strategy.routeRequest(key.get())
+                                                                     .subList(0, 1))
                                                  .contains(nodeId)) {
                                     writer.write(key.length());
                                     writer.write(key.get());
