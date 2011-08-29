a2efaf4dc15aa02f382f5d27a9e9ba4afc25abd6
Alex Feinberg <alex@strlen.net>
2011-08-28 20:46:31 -0700

    Add a unit test for CachedCallable

--- src/java/voldemort/utils/CachedCallable.java
+++ src/java/voldemort/utils/CachedCallable.java
@@ -11,21 +11,28 @@ public class CachedCallable<T> implements Callable<T> {
 
     private final Callable<T> inner;
     private final long ttlMs;
+    private final Time time;
     private final AtomicReference<T> valueRef;
 
     private volatile long lastCalledMs;
 
     public CachedCallable(Callable<T> inner,
                           long ttlMs) {
+        this(inner, ttlMs, SystemTime.INSTANCE);
+    }
+
+    public CachedCallable(Callable<T> inner,
+                          long ttlMs,
+                          Time time) {
         this.inner = inner;
         this.ttlMs = ttlMs;
+        this.time = time;
         valueRef = new AtomicReference<T>();
     }
 
-
     public T call() throws Exception {
         T value = valueRef.get();
-        long now = System.currentTimeMillis();
+        long now = time.getMilliseconds();
         if(value == null || now - lastCalledMs > ttlMs) {
             T newValue = inner.call();
             if(valueRef.compareAndSet(value, newValue)) {
