016989f15d58476685a1fe6be971f8679df099b6
Lei Gao <lgao@linkedin.com>
2011-09-09 12:33:38 -0700

    remove from the repo the files generated by rebalancing test

--- test/integration/voldemort/rebalance/test.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-source setup_env.inc
-
-  $vldm_dir/bin/voldemort-rebalance.sh --current-cluster $vldm_dir/config/test_config1/config/cluster.xml --current-stores $vldm_dir/config/test_config1/config/stores.xml --target-cluster $work_dir/targe-cluster.xml 
-
