acc16e7bb19100eed30bbbaf2a2ccf7be6a08b98
Chinmay Soman <csoman@linkedin.com>
2011-10-03 17:14:43 -0700

    Added testGetTimeout to RoutedStoreTest

--- test/unit/voldemort/store/routed/RoutedStoreTest.java
+++ test/unit/voldemort/store/routed/RoutedStoreTest.java
@@ -376,7 +376,7 @@ public class RoutedStoreTest extends AbstractByteArrayStoreTest {
 
     @Test
     public void testObsoleteMasterFails() {
-    // write me
+        // write me
     }
 
     @Test
@@ -1024,6 +1024,57 @@ public class RoutedStoreTest extends AbstractByteArrayStoreTest {
             routedStore.put(new ByteArray("test".getBytes()),
                             new Versioned<byte[]>(new byte[] { 1 }),
                             null);
+            fail("Should have thrown");
+        } catch(InsufficientOperationalNodesException e) {
+            long elapsed = (System.nanoTime() - start) / Time.NS_PER_MS;
+            assertTrue(elapsed + " < " + totalDelay, elapsed < totalDelay);
+        }
+    }
+
+    @Test
+    public void testGetTimeout() throws Exception {
+        int timeout = 50;
+        StoreDefinition definition = new StoreDefinitionBuilder().setName("test")
+                                                                 .setType("foo")
+                                                                 .setKeySerializer(new SerializerDefinition("test"))
+                                                                 .setValueSerializer(new SerializerDefinition("test"))
+                                                                 .setRoutingPolicy(RoutingTier.CLIENT)
+                                                                 .setRoutingStrategyType(RoutingStrategyType.CONSISTENT_STRATEGY)
+                                                                 .setReplicationFactor(3)
+                                                                 .setPreferredReads(3)
+                                                                 .setRequiredReads(3)
+                                                                 .setPreferredWrites(3)
+                                                                 .setRequiredWrites(3)
+                                                                 .build();
+        Map<Integer, Store<ByteArray, byte[], byte[]>> stores = new HashMap<Integer, Store<ByteArray, byte[], byte[]>>();
+        List<Node> nodes = new ArrayList<Node>();
+        int totalDelay = 0;
+        for(int i = 0; i < 3; i++) {
+            int delay = 4 + i * timeout;
+            totalDelay += delay;
+            Store<ByteArray, byte[], byte[]> store = new SleepyStore<ByteArray, byte[], byte[]>(delay,
+                                                                                                new InMemoryStorageEngine<ByteArray, byte[], byte[]>("test"));
+            stores.put(i, store);
+            List<Integer> partitions = Arrays.asList(i);
+            nodes.add(new Node(i, "none", 0, 0, 0, partitions));
+        }
+
+        setFailureDetector(stores);
+
+        routedStoreThreadPool = Executors.newFixedThreadPool(3);
+        RoutedStoreFactory routedStoreFactory = new RoutedStoreFactory(true,
+                                                                       routedStoreThreadPool,
+                                                                       timeout);
+
+        RoutedStore routedStore = routedStoreFactory.create(new Cluster("test", nodes),
+                                                            definition,
+                                                            stores,
+                                                            true,
+                                                            failureDetector);
+
+        long start = System.nanoTime();
+        try {
+            routedStore.get(new ByteArray("test".getBytes()), null);
             fail("Should have thrown");
         } catch(InsufficientOperationalNodesException e) {
             long elapsed = (System.nanoTime() - start) / Time.NS_PER_MS;
