1be2fe3ddbcb597b25e9e911c7f131d25a57c201
Karthik Kumar <oss.akk@gmail.com>
2011-12-30 17:44:29 -0800

    Fix ambiguity due to null - compilation error

--- test/unit/voldemort/utils/VoldemortIOUtilsTest.java
+++ test/unit/voldemort/utils/VoldemortIOUtilsTest.java
@@ -37,7 +37,7 @@ public class VoldemortIOUtilsTest {
 
     @Test
     public void testCloseQuietlyNullHttpResponse() {
-        VoldemortIOUtils.closeQuietly(null);
+        VoldemortIOUtils.closeQuietly((HttpResponse) null);
     }
 
     @Test
