349f852c93205c485aa6d7f3ad1f43cce1ce7220
Abhinay Nagpal <abhinaynagpal@gmail.com>
2012-11-27 15:56:22 -0800

    Changed release number

--- contrib/hadoop-store-builder/src/java/voldemort/store/readonly/mr/utils/MapperKeyValueWriter.java
+++ contrib/hadoop-store-builder/src/java/voldemort/store/readonly/mr/utils/MapperKeyValueWriter.java
@@ -120,12 +120,6 @@ public class MapperKeyValueWriter {
         List<Integer> partitionList = routingStrategy.getPartitionList(keyBytes);
         Node[] partitionToNode = routingStrategy.getPartitionToNode();
 
-        System.out.println("num partitions the key maps to is :" + partitionList.size());
-
-        for(int replicaType = 0; replicaType < partitionList.size(); replicaType++) {
-            System.out.println("key is hashed to node:"
-                               + partitionToNode[partitionList.get(replicaType)]);
-        }
         for(int replicaType = 0; replicaType < partitionList.size(); replicaType++) {
 
             // Node id
