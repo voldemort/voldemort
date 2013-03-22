667ed5038448090604e1d29078809c67a656cb2b
Jay J Wylie <jay.j.wylie@gmail.com>
2013-03-22 09:56:22 -0700

    Fix to log4j set up in bin/run-class.sh that should correctly configure log4j.

--- bin/run-class.sh
+++ bin/run-class.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #
-#   Copyright 2008-2009 LinkedIn, Inc
+#   Copyright 2008-2013 LinkedIn, Inc
 #
 #  Licensed under the Apache License, Version 2.0 (the "License");
 #   you may not use this file except in compliance with the License.
@@ -43,5 +43,8 @@ if [ -z "$VOLD_OPTS" ]; then
   VOLD_OPTS="-Xmx2G -server -Dcom.sun.management.jmxremote "
 fi
 
+# add '-Dlog4j.debug ' to debug log4j issues.
+LOG4JPROPERTIES="-Dlog4j.configuration=file:///${base_dir}/src/java/log4j.properties"
+
 export CLASSPATH
-java -Dlog4j.configuration=$base_dir/src/java/log4j.properties $VOLD_OPTS -cp $CLASSPATH $@
+java $LOG4JPROPERTIES $VOLD_OPTS -cp $CLASSPATH $@
