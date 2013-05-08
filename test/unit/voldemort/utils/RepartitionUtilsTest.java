cb5d292fdb313f07b442c9563cb7239d46a14890
Jay J Wylie <jay.j.wylie@gmail.com>
2013-05-08 08:38:43 -0700

    Additional minor refactoring of utils and utils tests.

--- test/unit/voldemort/utils/RepartitionUtilsTest.java
+++ test/unit/voldemort/utils/RepartitionUtilsTest.java
@@ -25,7 +25,6 @@ import java.util.Map;
 
 import org.junit.Test;
 
-import voldemort.ServerTestUtils;
 import voldemort.cluster.Cluster;
 import voldemort.store.StoreDefinition;
 
@@ -485,62 +484,6 @@ public class RepartitionUtilsTest {
         currentCluster = ClusterInstanceTest.getZZZCluster();
         storeDefs = ClusterInstanceTest.getZZZStoreDefsInMemory();
         decontigRepartition(currentCluster, storeDefs);
-    }
-
-    // TODO: Switch from numberOfZones to a set of zoneIds
-    // TODO: Create a ClusterTest or ClusterUtilsTest and add this test to that
-    // class/file
-    @Test
-    public void testGetMapOfContiguousPartitionRunLengths() {
-        int numberOfZones = 2;
-        int nodesPerZone[][] = new int[][] { { 0, 1, 2 }, { 3, 4, 5 } };
-        int partitionMap[][] = new int[][] { { 0, 6, 12, 16, 17 }, { 1, 7, 15 }, { 2, 8, 14 },
-                { 3, 9, 13 }, { 4, 10 }, { 5, 11 } };
-        Cluster cluster = ServerTestUtils.getLocalZonedCluster(numberOfZones,
-                                                               nodesPerZone,
-                                                               partitionMap);
-        Map<Integer, Integer> iiMap;
-        // Zone 0:
-        // 0, 1, 2, 6, 7, 8, 12, 14, 15, 16, 17
-        // -------oo-------oo--oo-------------- !Wraps around!
-
-        // => {14,7}, {6,3}, {12,1}
-        iiMap = ClusterUtils.getMapOfContiguousPartitions(cluster, 0);
-        assertTrue(iiMap.containsKey(6));
-        assertTrue(iiMap.get(6) == 3);
-        assertTrue(iiMap.containsKey(12));
-        assertTrue(iiMap.get(12) == 1);
-        assertTrue(iiMap.containsKey(14));
-        assertTrue(iiMap.get(14) == 7);
-
-        // => {3,1}, {1,1}, {7,1}
-        iiMap = ClusterUtils.getMapOfContiguousPartitionRunLengths(cluster, 0);
-        assertTrue(iiMap.containsKey(1));
-        assertTrue(iiMap.get(1) == 1);
-        assertTrue(iiMap.containsKey(3));
-        assertTrue(iiMap.get(3) == 1);
-        assertTrue(iiMap.containsKey(7));
-        assertTrue(iiMap.get(7) == 1);
-
-        // Zone 1:
-        // 3, 4, 5, 9, 10, 11, 13
-        // -------oo---------oo--
-
-        // => {3,3}, {9,3}, {13,1}
-        iiMap = ClusterUtils.getMapOfContiguousPartitions(cluster, 1);
-        assertTrue(iiMap.containsKey(3));
-        assertTrue(iiMap.get(3) == 3);
-        assertTrue(iiMap.containsKey(9));
-        assertTrue(iiMap.get(9) == 3);
-        assertTrue(iiMap.containsKey(13));
-        assertTrue(iiMap.get(13) == 1);
-
-        // => {1,1}, {3,2}
-        iiMap = ClusterUtils.getMapOfContiguousPartitionRunLengths(cluster, 1);
-        assertTrue(iiMap.containsKey(1));
-        assertTrue(iiMap.get(1) == 1);
-        assertTrue(iiMap.containsKey(3));
-        assertTrue(iiMap.get(3) == 2);
     }
 
 }
