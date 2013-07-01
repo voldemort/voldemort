e6fee031edc169d0e9611a306287683e8b121028
Siddharth Singh <sisingh@linkedin.com>
2013-07-01 14:56:26 -0700

    Expose max contiguous partitons option to script

--- bin/rebalance-shuffle.sh
+++ bin/rebalance-shuffle.sh
@@ -17,16 +17,21 @@
 
 # This script shuffles a current cluster.xml (to achieve a more balanced cluster) and then
 # outputs a final-cluster.xml corresponding to it. It also outputs the plan and the cost
-# to achieve this final cluster config.
+# to achieve this final cluster config. Eg.
 #
 # Argument = -c current_cluster -s current_stores -o output dir
 # The final cluster is placed in output_dir/
-
+#
+# The script also exposes an optional argument -m <max contiguous partitons>.
+# When invoked with the -m option the script first splits runs of partition that are bigger than
+# <max contiguous partitons> and then repartitions the cluster.
+#
+# Eg. Argument = -c current_cluster -s current_stores -o output dir -m 3
+#
 # This script can be used again on its own output. That is, if first attempt only gets you half-way 
 # to where you want to go in terms of repartitioning, then take output final-cluster xml and use as 
 # input to this tool again.
 #
-
 # This script uses getopts which means only single character switches are allowed.
 # Using getopt would allow for multi charcter switch names but would come at a
 # cost of not being cross compatible.
@@ -44,21 +49,23 @@ usage_and_exit() {
           under 'voldemort_home/config/tools/' for some store examples.
    -o     Output dir where all interim and final files will be stored.
           The directory will be created if it does not exist yet.
+   -m     Max allowed contiguous partition IDs within a zone (optional argument)
 EOF
 exit 1
 }
 
-# initialize  variables to an empty string
+# initialize  variables
 current_cluster=""
 current_stores=""
 output_dir=""
+max_contiguous_partitions=-1
 
 # Figure out voldemort home directory
 dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 vold_home="$(dirname "$dir")"
 
 # Parse options
-while getopts “hc:s:o:” OPTION
+while getopts “hc:s:o:m:” OPTION
 do
   case $OPTION in
   h)
@@ -77,6 +84,10 @@ do
     output_dir=$OPTARG
     mkdir -p $output_dir
     echo "[rebalance-shuffle] Using '$output_dir' for all interim and final files generated."
+    ;;
+  m)
+    max_contiguous_partitions=$OPTARG
+    echo "[rebalance-shuffle] Using '$max_contiguous_partitions' for max_contiguous_partitions."
     ;;
   ?)
     usage_and_exit
@@ -100,7 +111,11 @@ if [ ! -e $current_stores ]; then
     usage_and_exit "File '$current_stores' does not exist."
 fi
 
-# The final cluster.xml after shuffling is generated in two steps.
+# The final cluster.xml after shuffling is generated as described in the steps below:
+# Step 0 : This step is executed only if the max_contiguous_partitions is passed to the 
+#          script. In this step, lengthy runs of contiguous partitions are broken down
+#          to a maximum run of max_contiguous_partitions value. The final-cluster.xml
+#          from this step is then fed into step 1.
 # Step 1: Current cluster.xml is fed to the repartitioner along with random swap
 #         attempts. The repartitioner randomly swaps the partitions 
 #         and tries to balance the ring.
@@ -108,8 +123,27 @@ fi
 #         the one that is generated in step 1.
 #
 swap_attempts=250
+max_contiguous_attempts=50
 attempts=10
 swap_successes=250
+
+# Step 0
+if [ "$max_contiguous_partitions" -ne "-1" ]; then
+  mkdir -p $output_dir/step0/
+  $vold_home/bin/run-class.sh voldemort.tools.RepartitionerCLI \
+                            --current-cluster $current_cluster \
+                            --current-stores $current_stores \
+                            --max-contiguous-partitions $max_contiguous_partitions \
+                            --attempts $max_contiguous_attempts \
+                            --output-dir $output_dir/step0/
+
+  if [ ! -e $output_dir/step0/final-cluster.xml ]; then
+    usage_and_exit "File '$final-cluster.xml' does not exist."
+  fi
+
+  current_cluster=$output_dir/step0/final-cluster.xml
+  echo "[rebalance-shuffle] Will rebalance on the cluster in '$current_cluster'."
+fi
 
 # Step 1
 mkdir -p $output_dir/step1
