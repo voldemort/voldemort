cb938a050f929dd97c7228e05b84d65e0728142a
Siddharth Singh <sisingh@linkedin.com>
2013-07-01 13:06:32 -0700

    Copy plan outout to the final directory

--- bin/rebalance-shuffle.sh
+++ bin/rebalance-shuffle.sh
@@ -129,3 +129,5 @@ $vold_home/bin/run-class.sh voldemort.tools.RebalancePlanCLI \
                              
 echo "[rebalance-shuffle] Placing final-cluster.xml in '$output_dir'"
 cp $output_dir/step2/final-cluster.xml $output_dir/final-cluster.xml
+echo "[rebalance-shuffle] Placing plan.out in '$output_dir'"
+cp $output_dir/step2/plan.out $output_dir/plan.out
