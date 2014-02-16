44b9bc174a7afe542dfd3f276c57079c67aa0ed5
Abdullah Alansari <ahimta@gmail.com>
2014-02-16 18:47:46 +0300

    added: @Override to make unnecessary warnings go away in Props.java edited: PropsTest.java now is simpler and does, pretty much the same thing

--- test/unit/voldemort/utils/PropsTest.java
+++ test/unit/voldemort/utils/PropsTest.java
@@ -1,25 +1,27 @@
 package voldemort.utils;
 
 import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertThat;
+import static org.junit.Assume.assumeThat;
 
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Random;
 
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class PropsTest {
 
-    private final static int TIMES = 1000;
     private final static Random randomGenerator = new Random();
-    private final static int MAX_STRING_LENGTH = 20;
+    private final static int MAX_STRING_LENGTH = 10;
+    private final static int MAX_PROPS_SIZE = 10;
+    private final static int TIMES = 1000;
 
-    private static String randomString() {
+    /**
+     * 
+     * @return a {@code String} of length between {@literal 1} and
+     *         {@code MAX_STRING_LENGTH} inclusive
+     */
+    private static String generateString() {
         final int length = randomGenerator.nextInt(MAX_STRING_LENGTH) + 1;
         final StringBuilder builder = new StringBuilder(length);
 
@@ -31,177 +33,69 @@ public class PropsTest {
         return builder.toString();
     }
 
-    private static Map<String, String> randomMap() {
-        final int length = randomGenerator.nextInt();
-        final Map<String, String> map = new HashMap<String, String>();
+    /**
+     * 
+     * @return a {@code Props} of size between {@literal 1} and
+     *         {@code MAX_PROPS_LENGTH} inclusive
+     */
+    private static Props generateProps() {
+        final int length = randomGenerator.nextInt(MAX_PROPS_SIZE) + 1;
+        final Props props = new Props();
 
         for(int i = 0; i < length; i++) {
-            final String randomKey = randomString();
-            final String randomValue = randomString();
-
-            map.put(randomKey, randomValue);
+            props.put(generateString(), generateString());
         }
 
-        return map;
+        return props;
     }
 
-    @BeforeClass
-    public static void setUpBeforeClass() throws Exception {}
+    @Test
+    public void testClear_Remove_With_Size() {
+        for(int i = 0; i < TIMES; i++) {
+            final Props props = generateProps();
+            final String randomKey = generateString();
+            final String randomValue = generateString();
+            final int size = props.size();
+            final String key = props.keySet().iterator().next();
 
-    @AfterClass
-    public static void tearDownAfterClass() throws Exception {}
+            assertThat(props.with(randomKey, randomValue).get(randomKey), is(randomValue));
 
-    @Before
-    public void setUp() throws Exception {}
+            props.remove(key);
 
-    @After
-    public void tearDown() throws Exception {}
+            assumeThat(props.size(), is(size - 1));
+
+            assumeThat(props.size() > 0, is(true));
+            assumeThat(props.isEmpty(), is(false));
+
+            props.clear();
+
+            assumeThat(props.size() == 0, is(true));
+            assumeThat(props.isEmpty(), is(true));
+        }
+    }
 
     @Test
-    public void testHashCode() {}
-
-    @Test
-    public void testProps() {}
-
-    @Test
-    public void testPropsFileArray() {}
-
-    @Test
-    public void testPropsMapOfStringStringArray() {}
-
-    @Test
-    public void testPropsPropertiesArray() {}
-
-    @Test
-    public void testLoadProperties() {}
-
-    @Test
-    public void testClear() {}
-
-    @Test
-    public void testContainsKey() {
+    public void testContainsKey_ContainsValue_Get_Size() {
         Props props = new Props();
 
         for(int i = 0; i < TIMES; i++) {
-            final String randomKey = randomString();
-            final String randomValue = randomString();
+            final String randomKey = generateString();
+            final String randomValue = generateString();
+            final int size = props.size();
 
             props.put(randomKey, randomValue);
 
             assertThat(props.containsKey(randomKey), is(true));
             assertThat(props.containsKey(randomValue), is(false));
-        }
-    }
-
-    @Test
-    public void testContainsValue() {
-        Props props = new Props();
-
-        for(int i = 0; i < TIMES; i++) {
-            final String randomKey = randomString();
-            final String randomValue = randomString();
-
-            props.put(randomKey, randomValue);
 
             assertThat(props.containsValue(randomValue), is(true));
             assertThat(props.containsValue(randomKey), is(false));
+
+            assertThat(props.get(randomKey), is(randomValue));
+            assertNull(props.get(randomValue));
+
+            assertThat(props.size(), is(size + 1));
         }
     }
-
-    @Test
-    public void testEntrySet() {}
-
-    @Test
-    public void testGet() {}
-
-    @Test
-    public void testIsEmpty() {}
-
-    @Test
-    public void testKeySet() {}
-
-    @Test
-    public void testPutStringString() {}
-
-    @Test
-    public void testPutStringInteger() {}
-
-    @Test
-    public void testPutStringLong() {}
-
-    @Test
-    public void testPutStringDouble() {}
-
-    @Test
-    public void testWithStringString() {}
-
-    @Test
-    public void testWithStringInteger() {}
-
-    @Test
-    public void testWithStringDouble() {}
-
-    @Test
-    public void testWithStringLong() {}
-
-    @Test
-    public void testPutAll() {}
-
-    @Test
-    public void testRemove() {}
-
-    @Test
-    public void testSize() {}
-
-    @Test
-    public void testValues() {}
-
-    @Test
-    public void testGetStringStringString() {}
-
-    @Test
-    public void testGetStringString() {}
-
-    @Test
-    public void testGetBooleanStringBoolean() {}
-
-    @Test
-    public void testGetBooleanString() {}
-
-    @Test
-    public void testGetLongStringLong() {}
-
-    @Test
-    public void testGetLongString() {}
-
-    @Test
-    public void testGetIntStringInt() {}
-
-    @Test
-    public void testGetIntString() {}
-
-    @Test
-    public void testGetDoubleStringDouble() {}
-
-    @Test
-    public void testGetDoubleString() {}
-
-    @Test
-    public void testGetBytesStringLong() {}
-
-    @Test
-    public void testEqualsObject() {}
-
-    @Test
-    public void testToString() {}
-
-    @Test
-    public void testGetBytesString() {}
-
-    @Test
-    public void testGetListStringListOfString() {}
-
-    @Test
-    public void testGetListString() {}
 
 }
