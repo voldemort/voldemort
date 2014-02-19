d9ab0b59f95831937df29131178fedead561d94b
Saurabh Bhatia <srbhatia10@gmail.com>
2014-02-18 22:42:16 -0800

    Changed XmX1500M back to XmX2G in voldermort-server.sh

--- bin/voldemort-server.sh
+++ bin/voldemort-server.sh
@@ -42,7 +42,7 @@ done
 CLASSPATH=$CLASSPATH:$base_dir/dist/resources
 
 if [ -z "$VOLD_OPTS" ]; then
-  VOLD_OPTS="-Xmx1500M -server -Dcom.sun.management.jmxremote"
+  VOLD_OPTS="-Xmx2G -server -Dcom.sun.management.jmxremote"
 fi
 
 export CLASSPATH
