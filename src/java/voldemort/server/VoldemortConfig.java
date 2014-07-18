2c0707eefe168f82da9775fa2635c1a5b8733e38
Felix GV <fgv@linkedin.com>
2014-07-18 15:37:23 -0700

    Added basic functionality in RocksDbStorageConfiguration... untested yet.

--- src/java/voldemort/server/VoldemortConfig.java
+++ src/java/voldemort/server/VoldemortConfig.java
@@ -135,6 +135,8 @@ public class VoldemortConfig implements Serializable {
     private String mysqlHost;
     private int mysqlPort;
 
+    private String rdbDataDirectory;
+
     private int numReadOnlyVersions;
     private String readOnlyStorageDir;
     private String readOnlySearchStrategy;
@@ -566,6 +568,9 @@ public class VoldemortConfig implements Serializable {
                                                          Integer.MAX_VALUE);
         this.slopPurgeJobMaxKeysScannedPerSec = props.getInt("slop.purgejob.max.keys.scanned.per.sec",
                                                              10000);
+
+        // RocksDB config
+        this.rdbDataDirectory = props.getString("rdb.data.dir", "/tmp/rdb_data_dir");
 
         validateParams();
     }
@@ -3230,5 +3235,24 @@ public class VoldemortConfig implements Serializable {
     public String getFileFetcherClass() {
         return this.fileFetcherClass;
     }
+
+    public String getRdbDataDirectory() {
+      return rdbDataDirectory;
+    }
+
+  /**
+   * Where RocksDB should put its data directories
+   *
+   * <ul>
+   * <li>Property :"rdb.data.dir"</li>
+   * <li>Default : "/tmp/rdb_data_dir"</li>
+   * </ul>
+
+   * @param rdbDataDirectory
+   */
+    public void setRdbDataDirectory(String rdbDataDirectory) {
+      this.rdbDataDirectory = rdbDataDirectory;
+    }
+
 
 }
--- src/java/voldemort/store/rocksdb/RocksDbStorageConfiguration.java
+++ src/java/voldemort/store/rocksdb/RocksDbStorageConfiguration.java
@@ -1,18 +1,66 @@
 package voldemort.store.rocksdb;
 
+import org.apache.log4j.Logger;
+import org.rocksdb.*;
+import org.rocksdb.util.SizeUnit;
 import voldemort.routing.RoutingStrategy;
+import voldemort.server.VoldemortConfig;
 import voldemort.store.StorageConfiguration;
 import voldemort.store.StorageEngine;
+import voldemort.store.StorageInitializationException;
 import voldemort.store.StoreDefinition;
 import voldemort.utils.ByteArray;
+
+import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
 
 public class RocksDbStorageConfiguration implements StorageConfiguration {
 
   public static final String TYPE_NAME = "rocksdb";
 
+  private static Logger logger = Logger.getLogger(RocksDbStorageConfiguration.class);
+
+  private String rdbDataDirectory;
+
+  private Map<String, RocksDbStorageEngine> stores = new HashMap<String, RocksDbStorageEngine>();
+
+  public RocksDbStorageConfiguration(VoldemortConfig config) {
+    this.rdbDataDirectory = config.getRdbDataDirectory();
+  }
+
   @Override
   public StorageEngine<ByteArray, byte[], byte[]> getStore(StoreDefinition storeDef, RoutingStrategy strategy) {
-    return null;
+    String storeName = storeDef.getName();
+
+    if (!stores.containsKey(storeName)) {
+      String dataDir = rdbDataDirectory + "/" + storeName;
+
+      new File(dataDir).mkdirs();
+
+      // TODO: Validate those default mandatory options and make them configurable
+      Options rdbOptions = new Options().setCreateIfMissing(true)
+              .createStatistics()
+              .setWriteBufferSize(8 * SizeUnit.KB)
+              .setMaxWriteBufferNumber(3)
+              .setDisableSeekCompaction(true)
+              .setBlockSize(64 * SizeUnit.KB)
+              .setMaxBackgroundCompactions(10)
+              .setFilter(new BloomFilter(10))
+              .setCompressionType(CompressionType.SNAPPY_COMPRESSION);
+
+      try {
+        RocksDB rdbStore = RocksDB.open(rdbOptions, dataDir);
+
+        RocksDbStorageEngine rdbStorageEngine = new RocksDbStorageEngine(storeName, rdbStore);
+
+        stores.put(storeName, rdbStorageEngine);
+      } catch (Exception e) {
+        throw new StorageInitializationException(e);
+      }
+    }
+
+    return stores.get(storeName);
   }
 
   @Override
@@ -22,16 +70,24 @@ public class RocksDbStorageConfiguration implements StorageConfiguration {
 
   @Override
   public void update(StoreDefinition storeDef) {
-  
+    throw new UnsupportedOperationException();
   }
 
   @Override
   public void close() {
-  
+    for (RocksDbStorageEngine rdbStorageEngine : stores.values()) {
+      rdbStorageEngine.getRdb().close();
+    }
+
+    stores.clear();
   }
 
   @Override
   public void removeStorageEngine(StorageEngine<ByteArray, byte[], byte[]> engine) {
-  
+    RocksDbStorageEngine rdbStorageEngine = (RocksDbStorageEngine) engine;
+
+    rdbStorageEngine.getRdb().close();
+
+    stores.remove(rdbStorageEngine.getName());
   }
 }
