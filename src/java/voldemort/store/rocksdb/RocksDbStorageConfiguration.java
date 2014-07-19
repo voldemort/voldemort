54f8ed1569515248b95aae96573990821a80a635
Bhavani Sudha Saktheeswaran <bhavanisudhas@gmail.com>
2014-07-19 16:46:14 -0700

    Adding first impmenetations for getall and put APIs Also added common locking mechanism to be used for write operations

--- src/java/voldemort/store/rocksdb/RocksDbStorageConfiguration.java
+++ src/java/voldemort/store/rocksdb/RocksDbStorageConfiguration.java
@@ -1,8 +1,16 @@
 package voldemort.store.rocksdb;
 
+import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
+
 import org.apache.log4j.Logger;
-import org.rocksdb.*;
+import org.rocksdb.BloomFilter;
+import org.rocksdb.CompressionType;
+import org.rocksdb.Options;
+import org.rocksdb.RocksDB;
 import org.rocksdb.util.SizeUnit;
+
 import voldemort.routing.RoutingStrategy;
 import voldemort.server.VoldemortConfig;
 import voldemort.store.StorageConfiguration;
@@ -11,87 +19,94 @@ import voldemort.store.StorageInitializationException;
 import voldemort.store.StoreDefinition;
 import voldemort.utils.ByteArray;
 
-import java.io.File;
-import java.util.HashMap;
-import java.util.Map;
-
 public class RocksDbStorageConfiguration implements StorageConfiguration {
 
-  static {
-    RocksDB.loadLibrary();
-  }
-
-  public static final String TYPE_NAME = "rocksdb";
-
-  private static Logger logger = Logger.getLogger(RocksDbStorageConfiguration.class);
-
-  private String rdbDataDirectory;
-
-  private Map<String, RocksDbStorageEngine> stores = new HashMap<String, RocksDbStorageEngine>();
-
-  public RocksDbStorageConfiguration(VoldemortConfig config) {
-    this.rdbDataDirectory = config.getRdbDataDirectory();
-  }
-
-  @Override
-  public StorageEngine<ByteArray, byte[], byte[]> getStore(StoreDefinition storeDef, RoutingStrategy strategy) {
-    String storeName = storeDef.getName();
-
-    if (!stores.containsKey(storeName)) {
-      String dataDir = rdbDataDirectory + "/" + storeName;
-
-      new File(dataDir).mkdirs();
-
-      // TODO: Validate those default mandatory options and make them configurable
-      Options rdbOptions = new Options().setCreateIfMissing(true)
-              .createStatistics()
-              .setWriteBufferSize(8 * SizeUnit.KB)
-              .setMaxWriteBufferNumber(3)
-              .setDisableSeekCompaction(true)
-              .setBlockSize(64 * SizeUnit.KB)
-              .setMaxBackgroundCompactions(10)
-              .setFilter(new BloomFilter(10))
-              .setCompressionType(CompressionType.SNAPPY_COMPRESSION);
-
-      try {
-        RocksDB rdbStore = RocksDB.open(rdbOptions, dataDir);
-
-        RocksDbStorageEngine rdbStorageEngine = new RocksDbStorageEngine(storeName, rdbStore);
-
-        stores.put(storeName, rdbStorageEngine);
-      } catch (Exception e) {
-        throw new StorageInitializationException(e);
-      }
+    static {
+        RocksDB.loadLibrary();
     }
 
-    return stores.get(storeName);
-  }
+    private final int lockStripes;
 
-  @Override
-  public String getType() {
-    return TYPE_NAME;
-  }
+    public static final String TYPE_NAME = "rocksdb";
 
-  @Override
-  public void update(StoreDefinition storeDef) {
-    throw new UnsupportedOperationException();
-  }
+    private static Logger logger = Logger.getLogger(RocksDbStorageConfiguration.class);
 
-  @Override
-  public void close() {
-    for (RocksDbStorageEngine rdbStorageEngine : stores.values()) {
-      rdbStorageEngine.getRdb().close();
+    private String rdbDataDirectory;
+
+    private Map<String, RocksDbStorageEngine> stores = new HashMap<String, RocksDbStorageEngine>();
+
+    public RocksDbStorageConfiguration(VoldemortConfig config) {
+        /**
+         * - TODO 1. number of default locks need to debated. This default is
+         * same as that of Krati's. 2. Later add the property to VoldemortConfig
+         */
+        this.lockStripes = config.getAllProps().getInt("rocksdb.lock.stripes", 50);
+        this.rdbDataDirectory = config.getRdbDataDirectory();
     }
 
-    stores.clear();
-  }
+    @Override
+    public StorageEngine<ByteArray, byte[], byte[]> getStore(StoreDefinition storeDef,
+                                                             RoutingStrategy strategy) {
+        String storeName = storeDef.getName();
 
-  @Override
-  public void removeStorageEngine(StorageEngine<ByteArray, byte[], byte[]> engine) {
-    RocksDbStorageEngine rdbStorageEngine = (RocksDbStorageEngine) engine;
+        if(!stores.containsKey(storeName)) {
+            String dataDir = rdbDataDirectory + "/" + storeName;
 
-    rdbStorageEngine.getRdb().close();
+            new File(dataDir).mkdirs();
 
-    stores.remove(rdbStorageEngine.getName());
-  }
+            // TODO: Validate those default mandatory options and make them
+            // configurable
+            Options rdbOptions = new Options().setCreateIfMissing(true)
+                                              .createStatistics()
+                                              .setWriteBufferSize(8 * SizeUnit.KB)
+                                              .setMaxWriteBufferNumber(3)
+                                              .setDisableSeekCompaction(true)
+                                              .setBlockSize(64 * SizeUnit.KB)
+                                              .setMaxBackgroundCompactions(10)
+                                              .setFilter(new BloomFilter(10))
+                                              .setCompressionType(CompressionType.SNAPPY_COMPRESSION);
+
+            try {
+                RocksDB rdbStore = RocksDB.open(rdbOptions, dataDir);
+
+                RocksDbStorageEngine rdbStorageEngine = new RocksDbStorageEngine(storeName,
+                                                                                 rdbStore,
+                                                                                 lockStripes);
+
+                stores.put(storeName, rdbStorageEngine);
+            } catch(Exception e) {
+                throw new StorageInitializationException(e);
+            }
+        }
+
+        return stores.get(storeName);
+    }
+
+    @Override
+    public String getType() {
+        return TYPE_NAME;
+    }
+
+    @Override
+    public void update(StoreDefinition storeDef) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void close() {
+        for(RocksDbStorageEngine rdbStorageEngine: stores.values()) {
+            rdbStorageEngine.getRocksDB().close();
+        }
+
+        stores.clear();
+    }
+
+    @Override
+    public void removeStorageEngine(StorageEngine<ByteArray, byte[], byte[]> engine) {
+        RocksDbStorageEngine rdbStorageEngine = (RocksDbStorageEngine) engine;
+
+        rdbStorageEngine.getRocksDB().close();
+
+        stores.remove(rdbStorageEngine.getName());
+    }
 }
--- src/java/voldemort/store/rocksdb/RocksDbStorageEngine.java
+++ src/java/voldemort/store/rocksdb/RocksDbStorageEngine.java
@@ -1,16 +1,24 @@
 package voldemort.store.rocksdb;
 
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 
+import org.apache.commons.codec.binary.Hex;
 import org.rocksdb.RocksDB;
 import org.rocksdb.RocksDBException;
 
+import voldemort.VoldemortException;
 import voldemort.store.AbstractStorageEngine;
 import voldemort.store.PersistenceFailureException;
 import voldemort.store.StoreBinaryFormat;
 import voldemort.store.StoreUtils;
 import voldemort.utils.ByteArray;
+import voldemort.utils.StripedLock;
+import voldemort.versioning.ObsoleteVersionException;
+import voldemort.versioning.Occurred;
 import voldemort.versioning.Versioned;
 
 /**
@@ -20,17 +28,20 @@ import voldemort.versioning.Versioned;
  */
 public class RocksDbStorageEngine extends AbstractStorageEngine<ByteArray, byte[], byte[]> {
 
-    private RocksDB rdb;
+    private RocksDB rocksDB;
+    private final StripedLock locks;
+    private static final Hex hexCodec = new Hex();
 
     // TODO Need to add stats and loggers later
 
-    public RocksDbStorageEngine(String name, RocksDB rdbInstance) {
+    public RocksDbStorageEngine(String name, RocksDB rdbInstance, int lockStripes) {
         super(name);
-        this.rdb = rdbInstance;
+        this.rocksDB = rdbInstance;
+        this.locks = new StripedLock(lockStripes);
     }
 
-    public RocksDB getRdb() {
-        return rdb;
+    public RocksDB getRocksDB() {
+        return rocksDB;
     }
 
     @Override
@@ -40,7 +51,7 @@ public class RocksDbStorageEngine extends AbstractStorageEngine<ByteArray, byte[
         StoreUtils.assertValidKey(key);
         List<Versioned<byte[]>> value = null;
         try {
-            byte[] result = this.rdb.get(key.get());
+            byte[] result = getRocksDB().get(key.get());
             if(result != null) {
                 value = StoreBinaryFormat.fromByteArray(result);
             } else {
@@ -54,4 +65,66 @@ public class RocksDbStorageEngine extends AbstractStorageEngine<ByteArray, byte[
         return value;
     }
 
+    @Override
+    public Map<ByteArray, List<Versioned<byte[]>>> getAll(Iterable<ByteArray> keys,
+                                                          Map<ByteArray, byte[]> transforms)
+            throws VoldemortException {
+        // TODO Does RocksDB multiget supports atomicity ?
+        StoreUtils.assertValidKeys(keys);
+        Map<ByteArray, List<Versioned<byte[]>>> results = null;
+
+        try {
+            results = StoreUtils.getAll(this, keys, transforms);
+        } catch(PersistenceFailureException pfe) {
+            throw pfe;
+        } finally {
+            // TODO log time taken
+        }
+
+        return results;
+    }
+
+    @Override
+    public void put(ByteArray key, Versioned<byte[]> value, byte[] transforms)
+            throws PersistenceFailureException {
+        StoreUtils.assertValidKey(key);
+
+        synchronized(this.locks.lockFor(key.get())) {
+            // get the value
+            List<Versioned<byte[]>> currentValues = get(key, transforms);
+
+            if(currentValues.size() > 0) {
+                // compare vector clocks and throw out old ones, for updates
+                Iterator<Versioned<byte[]>> iter = currentValues.iterator();
+                while(iter.hasNext()) {
+                    Versioned<byte[]> curr = iter.next();
+                    Occurred occured = value.getVersion().compare(curr.getVersion());
+                    if(occured == Occurred.BEFORE) {
+                        throw new ObsoleteVersionException("Key "
+                                                           + new String(hexCodec.encode(key.get()))
+                                                           + " "
+                                                           + value.getVersion().toString()
+                                                           + " is obsolete, it is no greater than the current version of "
+                                                           + curr.getVersion().toString() + ".");
+                    } else if(occured == Occurred.AFTER) {
+                        iter.remove();
+                    }
+                }
+            } else {
+                // if value does not exist add the value from put request to
+                // existing values
+                currentValues = new ArrayList<Versioned<byte[]>>(1);
+            }
+            currentValues.add(value);
+
+            try {
+                getRocksDB().put(key.get(), StoreBinaryFormat.toByteArray(currentValues));
+            } catch(RocksDBException rocksdbException) {
+                throw new PersistenceFailureException(rocksdbException);
+            } finally {
+                // TODO logging
+            }
+        }
+
+    }
 }
