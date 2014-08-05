add55a9448af44c1f96c5b66589e0e0ec96dc6a6
Felix GV <fgv@linkedin.com>
2014-08-04 18:49:29 -0700

    Fixed a test in StatsTest to use the new RequestCounter API.

--- test/unit/voldemort/store/stats/StatsTest.java
+++ test/unit/voldemort/store/stats/StatsTest.java
@@ -125,7 +125,7 @@ public class StatsTest {
         Time mockTime = mock(Time.class);
 
         when(mockTime.getMilliseconds()).thenReturn(startTime);
-        RequestCounter rc = new RequestCounter(resetDurationMs, mockTime);
+        RequestCounter rc = new RequestCounter("tests.StatsTest.statsShowSpuriousValues", resetDurationMs, mockTime);
 
         // Add some new stats and verify they were calculated correctly
         rc.addRequest(100 * NS_PER_MS, 0, 1000, 100, 1);
