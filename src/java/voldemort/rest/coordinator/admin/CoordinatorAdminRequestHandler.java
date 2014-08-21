5aabfbee5146ee0d0d9d5b8099688d9649558a5c
Felix GV <fgv@linkedin.com>
2014-08-20 18:07:54 -0700

    Fixed the test code in CoordinatorAdminRequestHandler so that it responds to requests. Still just test code, not very advanced yet.

--- src/java/voldemort/rest/coordinator/admin/CoordinatorAdminRequestHandler.java
+++ src/java/voldemort/rest/coordinator/admin/CoordinatorAdminRequestHandler.java
@@ -19,9 +19,13 @@ package voldemort.rest.coordinator.admin;
 import static org.jboss.netty.handler.codec.http.HttpHeaders.Names.CONTENT_LENGTH;
 import static org.jboss.netty.handler.codec.http.HttpResponseStatus.BAD_REQUEST;
 import static org.jboss.netty.handler.codec.http.HttpResponseStatus.NO_CONTENT;
+import static org.jboss.netty.handler.codec.http.HttpResponseStatus.OK;
 import static org.jboss.netty.handler.codec.http.HttpVersion.HTTP_1_1;
 
+import org.apache.commons.io.output.ByteArrayOutputStream;
 import org.apache.log4j.Logger;
+import org.jboss.netty.buffer.ChannelBuffer;
+import org.jboss.netty.buffer.ChannelBuffers;
 import org.jboss.netty.channel.ChannelHandlerContext;
 import org.jboss.netty.channel.ExceptionEvent;
 import org.jboss.netty.channel.MessageEvent;
@@ -35,6 +39,8 @@ import org.jboss.netty.handler.codec.http.HttpResponse;
 import voldemort.rest.RestErrorHandler;
 import voldemort.rest.RestMessageHeaders;
 
+import java.io.IOException;
+
 public class CoordinatorAdminRequestHandler extends SimpleChannelHandler {
 
     public HttpRequest request;
@@ -43,53 +49,63 @@ public class CoordinatorAdminRequestHandler extends SimpleChannelHandler {
 
     @Override
     public void messageReceived(ChannelHandlerContext ctx, MessageEvent messageEvent) throws Exception {
-        if (!readingChunks) {
-            HttpRequest request = this.request = (HttpRequest) messageEvent.getMessage();
-            String requestURI = this.request.getUri();
-            if (logger.isDebugEnabled()) {
-                logger.debug("Request URI: " + requestURI);
-            }
-
-            if (request.isChunked()) {
-                readingChunks = true;
-            } else {
-                // Instantiate the appropriate error handler
-                HttpMethod httpMethod = request.getMethod();
-                if (httpMethod.equals(HttpMethod.GET)) {
-                    if(logger.isDebugEnabled()) {
-                        logger.debug("Received a Http GET request at " + System.currentTimeMillis() + " ms");
-                    }
-                    // handleGet()
-                } else if (httpMethod.equals(HttpMethod.POST)) {
-                    if (logger.isDebugEnabled()) {
-                        logger.debug("Recieved a Http POST request at " + System.currentTimeMillis() + " ms");
-                    }
-                    // handlePut
-                } else if (httpMethod.equals(HttpMethod.DELETE)) {
-                    if (logger.isDebugEnabled()) {
-                        logger.debug("Received a Http DELETE request at " + System.currentTimeMillis() + " ms");
-                    }
-                    // handleDelete
-                } else {
-                    String errorMessage = "Illegal Http request received at " + System.currentTimeMillis() + " ms";
-                    logger.error(errorMessage);
-                    RestErrorHandler.writeErrorResponse(messageEvent, BAD_REQUEST, errorMessage);
-                    return;
-                }
-            }
-        } else {
-            HttpChunk chunk = (HttpChunk) messageEvent.getMessage();
-            if (chunk.isLast()) {
-                readingChunks = false;
-            }
-        }
+        logger.info("messageReceived!! (omg)");
         sendResponse(messageEvent);
+//        if (!readingChunks) {
+//            HttpRequest request = this.request = (HttpRequest) messageEvent.getMessage();
+//            String requestURI = this.request.getUri();
+//            if (logger.isDebugEnabled()) {
+//                logger.debug("Request URI: " + requestURI);
+//            }
+//
+//            if (request.isChunked()) {
+//                readingChunks = true;
+//            } else {
+//                // Instantiate the appropriate error handler
+//                HttpMethod httpMethod = request.getMethod();
+//                if (httpMethod.equals(HttpMethod.GET)) {
+//                    if(logger.isDebugEnabled()) {
+//                        logger.debug("Received a Http GET request at " + System.currentTimeMillis() + " ms");
+//                    }
+//                    // handleGet()
+//                } else if (httpMethod.equals(HttpMethod.POST)) {
+//                    if (logger.isDebugEnabled()) {
+//                        logger.debug("Recieved a Http POST request at " + System.currentTimeMillis() + " ms");
+//                    }
+//                    // handlePut
+//                } else if (httpMethod.equals(HttpMethod.DELETE)) {
+//                    if (logger.isDebugEnabled()) {
+//                        logger.debug("Received a Http DELETE request at " + System.currentTimeMillis() + " ms");
+//                    }
+//                    // handleDelete
+//                } else {
+//                    String errorMessage = "Illegal Http request received at " + System.currentTimeMillis() + " ms";
+//                    logger.error(errorMessage);
+//                    RestErrorHandler.writeErrorResponse(messageEvent, BAD_REQUEST, errorMessage);
+//                    return;
+//                }
+//            }
+//        } else {
+//            HttpChunk chunk = (HttpChunk) messageEvent.getMessage();
+//            if (chunk.isLast()) {
+//                readingChunks = false;
+//            }
+//        }
+        //sendResponse(messageEvent);
     }
 
     public void sendResponse(MessageEvent messageEvent) {
-        HttpResponse response = new DefaultHttpResponse(HTTP_1_1, NO_CONTENT);
-        response.setHeader(CONTENT_LENGTH, "0");
-        response.setHeader(RestMessageHeaders.X_VOLD_VECTOR_CLOCK, "Sid");
+        HttpResponse response = new DefaultHttpResponse(HTTP_1_1, OK);
+        response.setHeader(CONTENT_LENGTH, "4");
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        try {
+            outputStream.write("allo".getBytes());
+        } catch (IOException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        }
+        ChannelBuffer responseContent = ChannelBuffers.dynamicBuffer();
+        responseContent.writeBytes(outputStream.toByteArray());
+        response.setContent(responseContent);
         messageEvent.getChannel().write(response);
         logger.info("Sent");
     }
