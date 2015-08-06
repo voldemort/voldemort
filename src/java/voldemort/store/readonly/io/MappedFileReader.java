8647f823c0a3080da3b3812090ae887e3d34873e
Greg Banks <gbanks@linkedin.com>
2015-08-06 08:30:11 -0700

    Simplify MappedFileReader, close Unix fd early
    Remove most of the members and make them local variables in the map()
    function which is the only place that uses them.  This simplifies the
    c'tor and means it cannot throw IOException anymore.
    
    It also means we close the Unix file descriptor used to create the
    mapping much earlier.  This will help reduce the load on file
    descriptors.

--- src/java/voldemort/store/readonly/io/MappedFileReader.java
+++ src/java/voldemort/store/readonly/io/MappedFileReader.java
@@ -17,22 +17,12 @@ import org.apache.log4j.Logger;
  */
 public class MappedFileReader implements Closeable {
 
-    private FileChannel channel;
-
-    private long length = 0;
-
     private Closer closer = new Closer();
 
     private File file;
 
-    private int fd;
-
     public File getFile() {
         return file;
-    }
-
-    public int getFd() {
-        return fd;
     }
 
     public boolean isClosed() {
@@ -46,40 +36,38 @@ public class MappedFileReader implements Closeable {
 
     private static final Logger log = Logger.getLogger(MappedFileReader.class);
 
-    private FileInputStream in;
-
     private MappedByteBuffer mappedByteBuffer = null;
 
-    public MappedFileReader(String path) throws IOException {
+    public MappedFileReader(String path) {
         this(new File(path));
     }
 
-    public MappedFileReader(File file) throws IOException {
-
-        init(file);
-
-    }
-
-    private void init(File file) throws IOException {
-
+    public MappedFileReader(File file) {
         this.file = file;
-
-        this.in = new FileInputStream(file);
-        this.channel = in.getChannel();
-        this.fd = Native.getFd(in.getFD());
-
-        this.length = file.length();
-
     }
 
     /**
-     * Read from this mapped file.
+     * Return a new MappedByteBuffer which can be used to read
+     * data from the file.  If setAutoLock is true, attempt to
+     * lock the file's data in memory.  Calling map() multiple
+     * times returns the same MappedByteBuffer.
      */
     public MappedByteBuffer map(boolean setAutoLock) throws IOException {
 
+	long length = -1;
         try {
 
             if(mappedByteBuffer == null) {
+
+		/* Sample the file length just before we mmap() the
+		 * file, as it may have changed size since the ctor. We
+		 * do need to remember the exact length we mmap()ed for
+		 * later when we unmap() to avoid leaking mappings, but
+		 * MappedByteBuffer does that for us.
+		 */
+		length = file.length();
+		FileInputStream in = new FileInputStream(file);
+		FileChannel channel = in.getChannel();
 
                 if(setAutoLock) {
                     closer.add(new MemLock(file, in.getFD(), 0, length));
@@ -89,6 +77,11 @@ public class MappedFileReader implements Closeable {
 
                 closer.add(new MappedByteBufferCloser(mappedByteBuffer));
 
+		/* On Unix-like systems we don't need to keep the file
+		 * descriptor around after we mmap()ed the file.  So
+		 * close it now. */
+		channel.close();
+		in.close();
             }
 
             return mappedByteBuffer;
@@ -111,9 +104,6 @@ public class MappedFileReader implements Closeable {
 
         if(closer.isClosed())
             return;
-
-        closer.add(channel);
-        closer.add(in);
 
         closer.close();
 
