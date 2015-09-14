2738b7c9ed03da1e58e0c8b5730f687efb1e8998
Felix GV <fgv@linkedin.com>
2015-09-14 15:53:34 -0700

    Fixed HdfsFetcher test code and trimmed some fat.

--- contrib/hadoop-store-builder/src/java/voldemort/store/readonly/fetcher/HdfsFetcher.java
+++ contrib/hadoop-store-builder/src/java/voldemort/store/readonly/fetcher/HdfsFetcher.java
@@ -16,13 +16,9 @@
 
 package voldemort.store.readonly.fetcher;
 
-import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.fs.CommonConfigurationKeys;
 import org.apache.hadoop.fs.FileStatus;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.security.UserGroupInformation;
-import org.apache.hadoop.security.authentication.client.AuthenticationException;
 import org.apache.log4j.Logger;
 import voldemort.VoldemortException;
 import voldemort.server.VoldemortConfig;
@@ -30,52 +26,49 @@ import voldemort.server.protocol.admin.AdminServiceRequestHandler;
 import voldemort.server.protocol.admin.AsyncOperationStatus;
 import voldemort.store.metadata.MetadataStore;
 import voldemort.store.readonly.FileFetcher;
-import voldemort.store.readonly.checksum.CheckSum;
 import voldemort.store.readonly.checksum.CheckSum.CheckSumType;
 import voldemort.store.readonly.mr.utils.HadoopUtils;
-import voldemort.utils.EventThrottler;
-import voldemort.utils.ExceptionUtils;
-import voldemort.utils.JmxUtils;
-import voldemort.utils.Time;
-import voldemort.utils.Utils;
+import voldemort.utils.*;
 
 import javax.management.ObjectName;
 import java.io.File;
 import java.io.IOException;
-import java.security.PrivilegedExceptionAction;
 import java.text.NumberFormat;
 import java.util.Map;
 import java.util.concurrent.atomic.AtomicInteger;
 
-/*
- * A fetcher that fetches the store files from HDFS
+/**
+ * A {@link FileFetcher} implementation that fetches the store files from HDFS
  */
 public class HdfsFetcher implements FileFetcher {
 
-    private static final Logger logger = Logger.getLogger(HdfsFetcher.class);
-
+    // Constants
     public static final String GZIP_FILE_EXTENSION = ".gz";
     public static final String INDEX_FILE_EXTENSION = ".index";
     public static final String DATA_FILE_EXTENSION = ".data";
     public static final String METADATA_FILE_EXTENSION = ".metadata";
-    private final Long maxBytesPerSecond, reportingIntervalBytes;
-    private final int bufferSize;
+
+    // Class-level state
+    private static final Logger logger = Logger.getLogger(HdfsFetcher.class);
     private static final AtomicInteger copyCount = new AtomicInteger(0);
-    private EventThrottler throttler = null;
-    private long retryDelayMs = 0;
-    private int maxAttempts = 0;
-    private VoldemortConfig voldemortConfig = null;
+
+    // Instance-level state
+    private final Long maxBytesPerSecond, reportingIntervalBytes;
+    private final int bufferSize, maxAttempts, maxVersionsStatsFile;
+    private final long retryDelayMs;
     private final boolean enableStatsFile;
-    private final int maxVersionsStatsFile;
+    private final EventThrottler throttler;
+    private final VoldemortConfig voldemortConfig;
 
     private static Boolean allowFetchOfFiles = false;
 
     /**
-     * this is the constructor invoked via reflection from
+     * This is the constructor invoked via reflection from
      * {@link AdminServiceRequestHandler#setFetcherClass(voldemort.server.VoldemortConfig)}
      */
     public HdfsFetcher(VoldemortConfig config) {
-        this(config.getReadOnlyFetcherMaxBytesPerSecond(),
+        this(config,
+             config.getReadOnlyFetcherMaxBytesPerSecond(),
              config.getReadOnlyFetcherReportingIntervalBytes(),
              config.getReadOnlyFetcherThrottlerInterval(),
              config.getFetcherBufferSize(),
@@ -84,12 +77,23 @@ public class HdfsFetcher implements FileFetcher {
              config.isReadOnlyStatsFileEnabled(),
              config.getReadOnlyMaxVersionsStatsFile(),
              config.getFetcherSocketTimeout());
-        this.voldemortConfig = config;
     }
 
-    // Test-only constructor
+
+    /**
+     * Test-only constructor, with some default config values, including:
+     *
+     * The Hadoop config path is set to empty string, which triggers a different code path
+     * in {@link HadoopUtils#getConfiguration(voldemort.server.VoldemortConfig, String)}.
+     *
+     * The keytab path is set to empty string, which triggers a different code path
+     * in {@link HadoopUtils#getHadoopFileSystem(voldemort.server.VoldemortConfig, String)}.
+     *
+     * TODO: Change visibility or otherwise ensure that only test code can use this...
+     */
     public HdfsFetcher() {
-        this((Long) null,
+        this(new VoldemortConfig(-1, ""), // Fake config with a bogus node ID and server config path
+             (Long) null,
              VoldemortConfig.REPORTING_INTERVAL_BYTES,
              VoldemortConfig.DEFAULT_FETCHER_THROTTLE_INTERVAL_WINDOW_MS,
              VoldemortConfig.DEFAULT_FETCHER_BUFFER_SIZE,
@@ -98,17 +102,21 @@ public class HdfsFetcher implements FileFetcher {
              true,
              50,
              VoldemortConfig.DEFAULT_FETCHER_SOCKET_TIMEOUT);
+        this.voldemortConfig.setHadoopConfigPath("");
+        this.voldemortConfig.setReadOnlyKeytabPath("");
     }
 
-    public HdfsFetcher(Long maxBytesPerSecond,
-                       Long reportingIntervalBytes,
-                       int throttlerIntervalMs,
-                       int bufferSize,
-                       int retryCount,
-                       long retryDelayMs,
-                       boolean enableStatsFile,
-                       int maxVersionsStatsFile,
-                       int socketTimeout) {
+    private HdfsFetcher(VoldemortConfig config,
+                        Long maxBytesPerSecond,
+                        Long reportingIntervalBytes,
+                        int throttlerIntervalMs,
+                        int bufferSize,
+                        int retryCount,
+                        long retryDelayMs,
+                        boolean enableStatsFile,
+                        int maxVersionsStatsFile,
+                        int socketTimeout) {
+        this.voldemortConfig = config;
         String throttlerInfo = "";
         if(maxBytesPerSecond != null && maxBytesPerSecond > 0) {
             this.maxBytesPerSecond = maxBytesPerSecond;
@@ -118,6 +126,7 @@ public class HdfsFetcher implements FileFetcher {
             throttlerInfo = "throttler with global rate = " + maxBytesPerSecond + " bytes / sec";
         } else {
             this.maxBytesPerSecond = null;
+            this.throttler = null;
             throttlerInfo = "no throttler";
         }
         this.reportingIntervalBytes = Utils.notNull(reportingIntervalBytes);
@@ -138,32 +147,12 @@ public class HdfsFetcher implements FileFetcher {
     }
 
     @Override
-    public File fetch(String source,
-                      String dest,
-                      AsyncOperationStatus status,
-                      String storeName,
-                      long pushVersion,
-                      MetadataStore metadataStore) throws IOException {
-        String hadoopConfigPath = "";
-        if (this.voldemortConfig != null) {
-            hadoopConfigPath = this.voldemortConfig.getHadoopConfigPath();
-        }
-        return fetch(source,
-                     dest,
-                     status,
-                     storeName,
-                     pushVersion,
-                     metadataStore,
-                     hadoopConfigPath);
-    }
-
     public File fetch(String sourceFileUrl,
                       String destinationFile,
                       AsyncOperationStatus status,
                       String storeName,
                       long pushVersion,
-                      MetadataStore metadataStore,
-                      String hadoopConfigPath) throws IOException {
+                      MetadataStore metadataStore) throws IOException {
 
         ObjectName jmxName = null;
         HdfsCopyStats stats = null;
@@ -290,7 +279,7 @@ public class HdfsFetcher implements FileFetcher {
         return maxAttempts;
     }
 
-    /*
+    /**
      * Main method for testing fetching
      */
     public static void main(String[] args) throws Exception {
@@ -299,14 +288,13 @@ public class HdfsFetcher implements FileFetcher {
                         + " url [keytab-location kerberos-username hadoop-config-path [destDir]]");
         String url = args[0];
 
-        String keytabLocation = "";
-        String kerberosUser = "";
-        String hadoopPath = "";
+        HdfsFetcher fetcher = new HdfsFetcher();
+
         String destDir = null;
         if(args.length >= 4) {
-            keytabLocation = args[1];
-            kerberosUser = args[2];
-            hadoopPath = args[3];
+            fetcher.voldemortConfig.setReadOnlyKeytabPath(args[1]);
+            fetcher.voldemortConfig.setReadOnlyKerberosUser(args[2]);
+            fetcher.voldemortConfig.setHadoopConfigPath(args[3]);
         }
         if(args.length >= 5)
             destDir = args[4];
@@ -314,99 +302,16 @@ public class HdfsFetcher implements FileFetcher {
         // for testing we want to be able to download a single file
         allowFetchOfFiles = true;
 
-        long maxBytesPerSec = 1024 * 1024 * 1024;
-
-        final Configuration config = new Configuration();
-        config.setInt("io.file.buffer.size", VoldemortConfig.DEFAULT_FETCHER_BUFFER_SIZE);
-        config.set("hadoop.rpc.socket.factory.class.ClientProtocol",
-                   ConfigurableSocketFactory.class.getName());
-        config.setInt("io.socket.receive.buffer", 1 * 1024 * 1024 - 10000);
-
-        FileSystem fs = null;
+        FileSystem fs = HadoopUtils.getHadoopFileSystem(fetcher.voldemortConfig, url);
         Path p = new Path(url);
-
-        boolean isHftpBasedFetch = url.length() > 4 && url.substring(0, 4).equals("hftp");
-        logger.info("URL : " + url + " and hftp protocol enabled = " + isHftpBasedFetch);
-
-        if(hadoopPath.length() > 0 && !isHftpBasedFetch) {
-            config.set("hadoop.security.group.mapping",
-                       "org.apache.hadoop.security.ShellBasedUnixGroupsMapping");
-
-            config.addResource(new Path(hadoopPath + "/core-site.xml"));
-            config.addResource(new Path(hadoopPath + "/hdfs-site.xml"));
-
-            String security = config.get(CommonConfigurationKeys.HADOOP_SECURITY_AUTHENTICATION);
-
-            if(security == null || !security.equals("kerberos")) {
-                logger.info("Security isn't turned on in the conf: "
-                            + CommonConfigurationKeys.HADOOP_SECURITY_AUTHENTICATION + " = "
-                            + config.get(CommonConfigurationKeys.HADOOP_SECURITY_AUTHENTICATION));
-                logger.info("Fix that.  Exiting.");
-                return;
-            } else {
-                logger.info("Security is turned on in the conf. Trying to authenticate ...");
-            }
-        }
-
-        try {
-
-            // Get the filesystem object
-            if(keytabLocation.length() > 0 && !isHftpBasedFetch) {
-                UserGroupInformation.setConfiguration(config);
-                UserGroupInformation.loginUserFromKeytab(kerberosUser, keytabLocation);
-
-                final Path path = p;
-                try {
-                    logger.debug("I've logged in and am now Doasing as "
-                                 + UserGroupInformation.getCurrentUser().getUserName());
-                    fs = UserGroupInformation.getCurrentUser()
-                                             .doAs(new PrivilegedExceptionAction<FileSystem>() {
-
-                                                 public FileSystem run() throws Exception {
-                                                     FileSystem fs = path.getFileSystem(config);
-                                                     return fs;
-                                                 }
-                                             });
-                } catch(InterruptedException e) {
-                    logger.error(e.getMessage());
-                } catch(Exception e) {
-                    logger.error("Got an exception while getting the filesystem object: ");
-                    logger.error("Exception class : " + e.getClass());
-                    e.printStackTrace();
-                    for(StackTraceElement et: e.getStackTrace()) {
-                        logger.error(et.toString());
-                    }
-                }
-            } else {
-                fs = p.getFileSystem(config);
-            }
-
-        } catch(IOException e) {
-            e.printStackTrace();
-            System.err.println("IOException in getting Hadoop filesystem object !!! Exiting !!!");
-            System.exit(-1);
-        } catch(Throwable te) {
-            te.printStackTrace();
-            logger.error("Error thrown while trying to get Hadoop filesystem");
-            System.exit(-1);
-        }
 
         FileStatus status = fs.listStatus(p)[0];
         long size = status.getLen();
-        HdfsFetcher fetcher = new HdfsFetcher(maxBytesPerSec,
-                                              VoldemortConfig.REPORTING_INTERVAL_BYTES,
-                                              VoldemortConfig.DEFAULT_FETCHER_THROTTLE_INTERVAL_WINDOW_MS,
-                                              VoldemortConfig.DEFAULT_FETCHER_BUFFER_SIZE,
-                                              5,
-                                              5000,
-                                              true,
-                                              50,
-                                              VoldemortConfig.DEFAULT_FETCHER_SOCKET_TIMEOUT);
         long start = System.currentTimeMillis();
         if(destDir == null)
             destDir = System.getProperty("java.io.tmpdir") + File.separator + start;
 
-        File location = fetcher.fetch(url, destDir, null, null, -1, null, hadoopPath);
+        File location = fetcher.fetch(url, destDir, null, null, -1, null);
 
         double rate = size * Time.MS_PER_SECOND / (double) (System.currentTimeMillis() - start);
         NumberFormat nf = NumberFormat.getInstance();
