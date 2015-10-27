voldemort/voldemort

commit d25d169862cc12e599bb1c916d684d89b5b4bfd4

Upgrade to Hadoop 2.3.0-cdh5.1.5 and Kerberos clean up.

contrib/hadoop-store-builder/src/java/voldemort/store/readonly/fetcher/HdfsFetcher.java
@@ -22,6 +22,7 @@ import org.apache.hadoop.fs.FileStatus;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.security.UserGroupInformation;
+import org.apache.hadoop.security.authentication.client.AuthenticationException;
 import org.apache.log4j.Logger;
 import voldemort.VoldemortException;
 import voldemort.server.VoldemortConfig;
@@ -32,6 +33,7 @@ import voldemort.store.readonly.FileFetcher;
 import voldemort.store.readonly.checksum.CheckSum;
 import voldemort.store.readonly.checksum.CheckSum.CheckSumType;
 import voldemort.utils.EventThrottler;
+import voldemort.utils.ExceptionUtils;
 import voldemort.utils.JmxUtils;
 import voldemort.utils.Time;
 import voldemort.utils.Utils;
@@ -68,9 +70,9 @@ public class HdfsFetcher implements FileFetcher {
     private final int maxVersionsStatsFile;
     private int socketTimeout;
 
-    public static final String FS_DEFAULT_NAME = "fs.default.name";
-
     private static Boolean allowFetchOfFiles = false;
+
+    private static UserGroupInformation currentHadoopUser;
 
     /**
      * this is the constructor invoked via reflection from
@@ -95,16 +97,9 @@ public class HdfsFetcher implements FileFetcher {
     public HdfsFetcher() {
         this((Long) null,
              VoldemortConfig.REPORTING_INTERVAL_BYTES,
-             VoldemortConfig.DEFAULT_FETCHER_BUFFER_SIZE);
-    }
-
-    // Test-only constructor
-    public HdfsFetcher(Long maxBytesPerSecond, Long reportingIntervalBytes, int bufferSize) {
-        this(maxBytesPerSecond,
-             reportingIntervalBytes,
              VoldemortConfig.DEFAULT_FETCHER_THROTTLE_INTERVAL_WINDOW_MS,
-             bufferSize,
-                "",
+             VoldemortConfig.DEFAULT_FETCHER_BUFFER_SIZE,
+             "",
              "",
              3,
              1000,
@@ -194,7 +189,7 @@ public class HdfsFetcher implements FileFetcher {
                     + HdfsFetcher.keytabPath + " , kerberos principal = "
                     + HdfsFetcher.kerberosPrincipal);
 
-        if(hadoopConfigPath.length() > 0 && !isHftpBasedFetch) {
+        if(hadoopConfigPath.length() > 0) {
 
             config.addResource(new Path(hadoopConfigPath + "/core-site.xml"));
             config.addResource(new Path(hadoopConfigPath + "/hdfs-site.xml"));
@@ -215,85 +210,62 @@ public class HdfsFetcher implements FileFetcher {
     }
 
     private FileSystem getHadoopFileSystem(String sourceFileUrl, String hadoopConfigPath)
-            throws IOException {
+            throws Exception {
         final Configuration config = getConfiguration(sourceFileUrl, hadoopConfigPath);
         final Path path = new Path(sourceFileUrl);
-        boolean isHftpBasedFetch = isHftpBasedPath(sourceFileUrl);
         FileSystem fs = null;
 
-        if(HdfsFetcher.keytabPath.length() > 0 && !isHftpBasedFetch) {
-
-            /*
-             * We're seeing intermittent errors while trying to get the Hadoop
-             * filesystem in a privileged doAs block. This happens when we fetch
-             * the files over hdfs or webhdfs. This retry loop is inserted here
-             * as a temporary measure.
-             */
-            for(int attempt = 0; attempt < maxAttempts; attempt++) {
-                boolean isValidFilesystem = false;
-
-                if(!new File(HdfsFetcher.keytabPath).exists()) {
-                    logger.error("Invalid keytab file path. Please provide a valid keytab path");
-                    throw new VoldemortException("Error in getting Hadoop filesystem. Invalid keytab file path.");
-                }
-
-                /*
-                 * The Hadoop path for getting a Filesystem object in a
-                 * privileged doAs block is not thread safe. This might be
-                 * causing intermittent NPE exceptions. Adding a synchronized
-                 * block.
-                 */
-                synchronized(this) {
-                    /*
-                     * First login using the specified principal and keytab file
-                     */
-                    UserGroupInformation.setConfiguration(config);
-                    UserGroupInformation.loginUserFromKeytab(HdfsFetcher.kerberosPrincipal,
-                                                             HdfsFetcher.keytabPath);
-
-                    /*
-                     * If login is successful, get the filesystem object. NOTE:
-                     * Ideally we do not need a doAs block for this. Consider
-                     * removing it in the future once the Hadoop jars have the
-                     * corresponding patch (tracked in the Hadoop Apache
-                     * project: HDFS-3367)
-                     */
-                    try {
-                        logger.info("I've logged in and am now Doasing as "
-                                    + UserGroupInformation.getCurrentUser().getUserName());
-                        fs = UserGroupInformation.getCurrentUser()
-                                                 .doAs(new PrivilegedExceptionAction<FileSystem>() {
-
-                                                     @Override
-                                                     public FileSystem run() throws Exception {
-                                                         FileSystem fs = path.getFileSystem(config);
-                                                         return fs;
-                                                     }
-                                                 });
-                        isValidFilesystem = true;
-                    } catch(InterruptedException e) {
-                        logger.error(e.getMessage(), e);
-                    } catch(Exception e) {
-                        logger.error("Got an exception while getting the filesystem object: ");
-                        logger.error("Exception class : " + e.getClass());
-                        e.printStackTrace();
-                        for(StackTraceElement et: e.getStackTrace()) {
-                            logger.error(et.toString());
+        for (int attempt = 1; attempt <= maxAttempts; attempt++) {
+            try {
+                if (HdfsFetcher.keytabPath.length() > 0) {
+                    // UserGroupInformation.loginUserFromKeytab() should only need to happen once during
+                    // the lifetime of the JVM. We try to minimize login operations as much as possible,
+                    // but we will redo it if an AuthenticationException is caught below.
+                    synchronized (HdfsFetcher.class) {
+                        // The null check within the synchronized block is for two reasons:
+                        // 1- To minimize the amount of login operations from concurrent pushes.
+                        // 2- To prevent NPEs if the currentHadoopUser is reset to null in the catch block.
+                        if (currentHadoopUser == null) {
+                            if (!new File(HdfsFetcher.keytabPath).exists()) {
+                                logger.error("Invalid keytab file path. Please provide a valid keytab path");
+                                throw new VoldemortException("Error in getting Hadoop filesystem. Invalid keytab file path.");
+                            }
+                            UserGroupInformation.setConfiguration(config);
+                            UserGroupInformation.loginUserFromKeytab(HdfsFetcher.kerberosPrincipal, HdfsFetcher.keytabPath);
+                            currentHadoopUser = UserGroupInformation.getCurrentUser();
+                            logger.info("I have logged in as " + currentHadoopUser.getUserName());
+                        } else {
+                            // reloginFromKeytab() will not actually do anything unless the token is close to expiring.
+                            currentHadoopUser.reloginFromKeytab();
                         }
                     }
                 }
 
-                if(isValidFilesystem) {
-                    break;
-                } else if(attempt < maxAttempts - 1) {
-                    logger.error("Attempt#" + attempt
-                                 + " Could not get a valid Filesystem object. Trying again in "
-                                 + retryDelayMs + " ms");
-                    sleepForRetryDelayMs();
+                fs = path.getFileSystem(config);
+
+                // Just a small operation to make sure the FileSystem instance works.
+                fs.exists(path);
+
+                // Congrats for making it this far. Pass go and collect $200.
+                break;
+            } catch(VoldemortException e) {
+                // We only intend to catch and retry Hadoop-related exceptions, not Voldemort ones.
+                throw e;
+            } catch(Exception e) {
+                if (ExceptionUtils.recursiveClassEquals(e, AuthenticationException.class)) {
+                    logger.info("Got an AuthenticationException from HDFS. " +
+                            "Will retry to login from scratch, on next attempt.", e);
+                    synchronized (HdfsFetcher.class) {
+                        // Synchronized to prevent NPEs in the other synchronized block, above.
+                        currentHadoopUser = null;
+                    }
+                }
+                if(attempt < maxAttempts) {
+                    sleepForRetryDelayMs(attempt);
+                } else {
+                    throw e;
                 }
             }
-        } else {
-            fs = path.getFileSystem(config);
         }
         return fs;
     }
@@ -308,9 +280,10 @@ public class HdfsFetcher implements FileFetcher {
 
         ObjectName jmxName = null;
         HdfsCopyStats stats = null;
+        FileSystem fs = null;
         try {
 
-            final FileSystem fs = getHadoopFileSystem(sourceFileUrl, hadoopConfigPath);
+            fs = getHadoopFileSystem(sourceFileUrl, hadoopConfigPath);
             final Path path = new Path(sourceFileUrl);
             File destination = new File(destinationFile);
 
@@ -329,8 +302,6 @@ public class HdfsFetcher implements FileFetcher {
                                       new HdfsPathInfo(fs, path));
             jmxName = JmxUtils.registerMbean("hdfs-copy-" + copyCount.getAndIncrement(), stats);
 
-
-
             logger.info("Starting fetch for : " + sourceFileUrl);
             boolean result =
                     fetch(fs,
@@ -343,21 +314,16 @@ public class HdfsFetcher implements FileFetcher {
                           metadataStore);
             logger.info("Completed fetch : " + sourceFileUrl);
 
-            // Close the filesystem
-            fs.close();
-
             if(result) {
                 return destination;
             } else {
                 return null;
             }
-        } catch(Throwable te) {
-            logger.error("Error thrown while trying to get data from Hadoop filesystem", te);
+        } catch (Exception e) {
             if(stats != null) {
-                stats.reportError("File fetcher failed for destination " + destinationFile, te);
+                stats.reportError("File fetcher failed for destination " + destinationFile, e);
             }
-            throw new VoldemortException("Error thrown while trying to get data from Hadoop filesystem : "
-                                         + te);
+            throw new VoldemortException("Error thrown while trying to get data from Hadoop filesystem : ", e);
 
         } finally {
             if(jmxName != null)
@@ -366,15 +332,33 @@ public class HdfsFetcher implements FileFetcher {
             if(stats != null) {
                 stats.complete();
             }
+
+            if (fs != null) {
+                try {
+                    fs.close();
+                } catch (IOException e) {
+                    String errorMessage = "Got IOException while trying to close the filesystem instance (harmless).";
+                    if(stats != null) {
+                        stats.reportError(errorMessage, e);
+                    }
+                    logger.info(errorMessage, e);
+                }
+            }
         }
     }
 
-    private void sleepForRetryDelayMs() {
+    private void sleepForRetryDelayMs(int attemptNumber) {
         if(retryDelayMs > 0) {
+            // Doing random back off so that all nodes do not end up swarming the KDC infra
+            long randomDelay = (long) (Math.random() * retryDelayMs + retryDelayMs);
+
+            logger.error("Could not get a valid Filesystem object on attempt # " + attemptNumber +
+                    " / " + maxAttempts + ". Trying again in " + randomDelay + " ms.");
             try {
                 Thread.sleep(retryDelayMs);
             } catch(InterruptedException ie) {
                 logger.error("Fetcher interrupted while waiting to retry", ie);
+                Thread.currentThread().interrupt();
             }
         }
     }

build.gradle
@@ -56,6 +56,10 @@ compileJava.options.debug = true
 
 repositories {
     mavenCentral()
+    maven {
+        // For Hadoop dependencies
+        url "https://repository.cloudera.com/artifactory/cloudera-repos/"
+    }
     flatDir { dirs privateLibDir }
     flatDir { dirs contribRootDir }
 }
@@ -393,7 +397,7 @@ dependencies {
     compile 'com.google.guava:guava:14.0.1'
 
     // used for readonly store hdfs fetcher.
-    compile 'org.apache.hadoop:hadoop-auth:2.0.5-alpha'
+    compile 'org.apache.hadoop:hadoop-auth:2.3.0-cdh5.1.5'
 
     // used at lots of places. Seems like there is some overlap between httpclient and core, but not clear
     compile 'org.apache.httpcomponents:httpclient:4.1.2'
@@ -409,17 +413,6 @@ dependencies {
 
     // Used for reading XML files and Document.
     compile 'jdom:jdom:1.1'
-
-    // je- BDB je 5.0.88 is not available in maven central repository.
-    // Could be possibly found in oracle maven repository, but not sure
-    /*
-     groupId>com.sleepycat</groupId>
-     <artifactId>je</artifactId>
-     <version>3.3.75</version>
-     groupId>com.sleepycat</groupId>
-     <artifactId>je</artifactId>
-     <version>3.3.75</version>
-     */
 
     // Jetty is used for HttpService and tests. Jetty Util is used for QueuedThreadPool class.
     compile 'org.mortbay.jetty:jetty-util:6.1.18'
@@ -439,11 +432,6 @@ dependencies {
 
     // Used for argument command line parsing
     compile 'net.sf.jopt-simple:jopt-simple:4.6'
-
-    // INTERNAL_LIBS thrift is one of the supported serialization format.
-    // libthrift 0.5 version is not found in the maven central repository.
-    // The closest version available is 0.6.1
-    // libthrift-0.5.0.jar
 
     // log4j - logger used in almost all files
     compile 'log4j:log4j:1.2.15'
@@ -507,7 +495,18 @@ dependencies {
     // conflict resolution is not done properly across sourceSets
     // and we end up with 2 versions of few jars like ( log4j, servlet etc. )
     compile 'commons-configuration:commons-configuration:1.6'
-    compile 'org.apache.hadoop:hadoop-core:1.2.1'
+    compile('org.apache.hadoop:hadoop-core:2.3.0-mr1-cdh5.1.5') {
+        exclude group: 'com.google.protobuf'
+        exclude group: 'org.apache.avro'
+    }
+    compile('org.apache.hadoop:hadoop-common:2.3.0-cdh5.1.5') {
+        exclude group: 'com.google.protobuf'
+        exclude group: 'org.apache.avro'
+    }
+    compile('org.apache.hadoop:hadoop-hdfs:2.3.0-cdh5.1.5') {
+        exclude group: 'com.google.protobuf'
+        exclude group: 'org.apache.avro'
+    }
 
     compile 'com.linkedin.pegasus:r2:1.8.3'
     compile 'com.linkedin.pegasus:data:1.8.3'

contrib/hadoop-store-builder/src/java/voldemort/store/readonly/mr/azkaban/VoldemortBuildAndPushJob.java
@@ -132,6 +132,8 @@ public class VoldemortBuildAndPushJob extends AbstractJob {
     public final static String MIN_NUMBER_OF_RECORDS = "min.number.of.records";
     public final static String REDUCER_OUTPUT_COMPRESS_CODEC = "reducer.output.compress.codec";
     public final static String REDUCER_OUTPUT_COMPRESS = "reducer.output.compress";
+    // default
+    private final static String RECOMMENDED_FETCHER_PROTOCOL = "webhdfs";
 
     // CONFIG VALUES (and other immutable state)
     private final Props props;
@@ -200,7 +202,11 @@ public class VoldemortBuildAndPushJob extends AbstractJob {
 
         this.nodeId = props.getInt(PUSH_NODE, 0);
 
-        this.hdfsFetcherProtocol = props.getString(VOLDEMORT_FETCHER_PROTOCOL, "hftp");
+        this.hdfsFetcherProtocol = props.getString(VOLDEMORT_FETCHER_PROTOCOL, RECOMMENDED_FETCHER_PROTOCOL);
+        if (this.hdfsFetcherProtocol != RECOMMENDED_FETCHER_PROTOCOL) {
+            log.warn("It is recommended to use the " + RECOMMENDED_FETCHER_PROTOCOL + " protocol only.");
+        }
+
         this.hdfsFetcherPort = props.getString(VOLDEMORT_FETCHER_PORT, "50070");
 
         log.info(VOLDEMORT_FETCHER_PROTOCOL + " is set to : " + hdfsFetcherProtocol);

src/java/voldemort/server/VoldemortConfig.java
@@ -2877,9 +2877,9 @@ public class VoldemortConfig implements Serializable {
     }
 
     /**
-     * Amount of delay in ms between readonly fetcher retries, to fetch data
-     * from Hadoop
-     * 
+     * Minimum delay in ms between readonly fetcher retries, to fetch data
+     * from Hadoop. The maximum delay is 2x this amount, determined randomly.
+     *
      * <ul>
      * <li>Property :"fetcher.retry.delay.ms"</li>
      * <li>Default :5000 (5 seconds)</li>

src/java/voldemort/utils/ExceptionUtils.java
@@ -1,0 +1,15 @@
+package voldemort.utils;
+
+/**
+ * Static utility functions to deal with exceptions.
+ */
+public class ExceptionUtils {
+    public static boolean recursiveClassEquals(Throwable throwableToInspect, Class throwableToLookFor) {
+        if (throwableToInspect.getClass().equals(throwableToLookFor)) {
+            return true;
+        } else {
+            Throwable cause = throwableToInspect.getCause();
+            return cause != null && recursiveClassEquals(cause, throwableToLookFor);
+        }
+    }
+}

commit 576e61d63c92d53e86058bcd1a7dbba0f1476d5e

Fixed regression introduced in d25d169862cc12e599bb1c916d684d89b5b4bfd4.
The Read-Only servers used to percolate exception messages back to Build and Push jobs, for easy debugging of Hadoop-related exceptions (i.e.: AccessControlException). As part of the commit above, the server begun swallowing the underlying exceptions' message, which makes debugging BnP jobs harder. This commit fixes the regression.

Also included some minor clean ups, such as JavaDoc improvements and @Deprecated annotations for the test-only functions.

contrib/hadoop-store-builder/src/java/voldemort/store/readonly/fetcher/HdfsFetcher.java
@@ -104,8 +104,11 @@ public class HdfsFetcher implements FileFetcher {
      * The keytab path is set to empty string, which triggers a different code path
      * in {@link HadoopUtils#getHadoopFileSystem(voldemort.server.VoldemortConfig, String)}.
      *
-     * TODO: Change visibility or otherwise ensure that only test code can use this...
+     * FIXME: Change visibility or otherwise ensure that only test code can use this...
+     *
+     * @deprecated Do not use for production code, use {@link #HdfsFetcher(voldemort.server.VoldemortConfig)} instead.
      */
+    @Deprecated
     public HdfsFetcher() {
         this(new VoldemortConfig(-1, ""), // Fake config with a bogus node ID and server config path
              (Long) null,
@@ -157,19 +160,30 @@ public class HdfsFetcher implements FileFetcher {
                 ", fetcher socket timeout = " + socketTimeout + " ms.");
     }
 
+    /**
+     * Used only by unit tests and by the deprecated {@link voldemort.server.http.gui.ReadOnlyStoreManagementServlet}.
+     *
+     * FIXME: Refactor test code with dependency injection or scope restrictions so this function is not public.
+     *
+     * @deprecated Do not use for production code, use {@link #fetch(String, String, voldemort.server.protocol.admin.AsyncOperationStatus, String, long, voldemort.store.metadata.MetadataStore)} instead.
+     */
+    @Deprecated
     @Override
     public File fetch(String source, String dest) throws Exception {
         return fetch(source, dest, VoldemortConfig.DEFAULT_STORAGE_SPACE_QUOTA_IN_KB);
     }
 
+    /**
+     * Used for unit tests only.
+     *
+     * FIXME: Refactor test code with dependency injection or scope restrictions so this function is not public.
+     *
+     * @deprecated Do not use for production code, use {@link #fetch(String, String, voldemort.server.protocol.admin.AsyncOperationStatus, String, long, voldemort.store.metadata.MetadataStore)} instead.
+     */
+    @Deprecated
     @Override
-    public File fetch(String source, String dest, long diskQuotaSizeInKB) throws IOException,
-            Exception {
-        return fetchFromSource(source, dest,
-                                   null,
-                                   null,
-                                   -1,
-                                   diskQuotaSizeInKB);
+    public File fetch(String source, String dest, long diskQuotaSizeInKB) throws Exception {
+        return fetchFromSource(source, dest, null, null, -1, diskQuotaSizeInKB);
     }
 
     @Override
@@ -186,10 +200,9 @@ public class HdfsFetcher implements FileFetcher {
                                           new ClientConfig());
 
             Versioned<String> diskQuotaSize = adminClient.quotaMgmtOps.getQuotaForNode(storeName,
-                                                                                           QuotaType.STORAGE_SPACE,
-                                                                                           metadataStore.getNodeId());
-            Long diskQuoataSizeInKB = (diskQuotaSize == null) ? null
-                                                             : (Long.parseLong(diskQuotaSize.getValue()));
+                                                                                       QuotaType.STORAGE_SPACE,
+                                                                                       metadataStore.getNodeId());
+            Long diskQuoataSizeInKB = (diskQuotaSize == null) ? null : (Long.parseLong(diskQuotaSize.getValue()));
             logger.info("Starting fetch for : " + sourceFileUrl);
             return fetchFromSource(sourceFileUrl,
                                    destinationFile,
@@ -299,18 +312,15 @@ public class HdfsFetcher implements FileFetcher {
                 logger.error("Source " + path.toString() + " should be a directory");
                 return null;
             }
-        } catch(Exception e) {
+        } catch (Exception e) {
             if(stats != null) {
                 stats.reportError("File fetcher failed for destination " + destinationFile, e);
             }
-            String errorMessage = "Error thrown while trying to get data from Hadoop filesystem : ";
-            logger.error(errorMessage, e);
             if(e instanceof VoldemortException) {
                 throw e;
             } else {
-                throw new VoldemortException(errorMessage, e);
+                throw new VoldemortException("Error thrown while trying to get data from Hadoop filesystem: " + e.getMessage(), e);
             }
-
         } finally {
             if(jmxName != null)
                 JmxUtils.unregisterMbean(jmxName);