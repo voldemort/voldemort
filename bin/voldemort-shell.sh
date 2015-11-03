a9cd43215cab412674766f7e7f34d7b15dec1f66
Brendan Harris <bharris@linkedin.com>
2015-11-02 17:43:27 -0800

    Removing the redundant argument check so that the VoldemortClientShell.java option parser can be used to query help

--- bin/voldemort-shell.sh
+++ bin/voldemort-shell.sh
@@ -16,12 +16,6 @@
 #  limitations under the License.
 #
 
-if [ $# -lt 2 ];
-then
-	echo 'USAGE: bin/voldemort-shell.sh store_name bootstrap_url [command_file] [--client-zone-id <zone-id>]'
-	exit 1
-fi
-
 base_dir=$(dirname $0)/..
 
 $base_dir/bin/run-class.sh jline.ConsoleRunner voldemort.VoldemortClientShell $@
