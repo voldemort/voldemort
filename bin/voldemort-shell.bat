8c6dcf82e94e8801c97b454fb6e92eba19b3505b
Brendan Harris <bharris@linkedin.com>
2015-11-02 17:43:27 -0800

    Removing the redundant argument check so that the VoldemortClientShell.java option parser can be used to query help
    Removing the redundant argument check so that the VoldemortClientShell.java option parser can be used to query help
    
    Augmented help and added an option for passing in a properties file
    
    The shell now passes in an option that tells VoldemortClientShell that it is being called from voldemort-shell.sh so that it can properly format the help output
    
    The shell now passes in an option that tells VoldemortClientShell that it is being called from voldemort-shell.sh so that it can properly format the help output
    
    Made help for --voldemort-shell option more 'helpful'
    
    Mistakenly bumped up a greaterthan check on the positional arguments ... Rolling that back
    
    Rewording --help helper text to be more helpful

--- bin/voldemort-shell.bat
+++ bin/voldemort-shell.bat
@@ -17,17 +17,10 @@ REM limitations under the License.
 REM
 REM ** This Windows BAT file is not tested with each Voldemort release. **
 
-set argC=0
-for %%a in (%*) do set /a argC+=1
-if %argC% geq 2 goto :continue
-echo "USAGE: bin/voldemort-shell.bat store_name bootstrap_url [command_file] [--client-zone-id <zone-id>]"
-goto :eof
-:continue
-
 setlocal
 SET BASE_DIR=%~dp0..
 
-call "%BASE_DIR%\bin\run-class.bat" jline.ConsoleRunner voldemort.VoldemortClientShell %*
+call "%BASE_DIR%\bin\run-class.bat" jline.ConsoleRunner voldemort.VoldemortClientShell %* --voldemort-shell bat
 
 endlocal
 
