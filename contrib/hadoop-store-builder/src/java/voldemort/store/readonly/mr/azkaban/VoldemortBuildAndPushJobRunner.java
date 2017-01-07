ba9386b00b60c936856aeedc0def8bbf54ca08b1
Gaojie Liu <goliu@linkedin.com>
2017-01-06 16:08:53 -0800

    Fixed a compilation error introduced by last commit.

--- contrib/hadoop-store-builder/src/java/voldemort/store/readonly/mr/azkaban/VoldemortBuildAndPushJobRunner.java
+++ contrib/hadoop-store-builder/src/java/voldemort/store/readonly/mr/azkaban/VoldemortBuildAndPushJobRunner.java
@@ -10,7 +10,7 @@ import java.io.IOException;
  */
 public class VoldemortBuildAndPushJobRunner {
     private static final Logger logger = Logger.getLogger(VoldemortBuildAndPushJobRunner.class);
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         // Validate arguments
         if (args.length < 1) {
             logger.error("Please provide a job config file name as the argument to this script.");
