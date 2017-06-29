6aff0edd7c3801a77ac18bf96e9cc589b58fbe2a
Charles Gao <chgao@linkedin.com>
2017-06-28 23:52:44 -0700

    Added optional CDN feature to the BnP pipeline.

--- contrib/hadoop-store-builder/src/java/voldemort/store/readonly/mr/azkaban/VoldemortSwapJob.java
+++ contrib/hadoop-store-builder/src/java/voldemort/store/readonly/mr/azkaban/VoldemortSwapJob.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 2008-2013 LinkedIn, Inc
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
  * the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
@@ -24,8 +24,12 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
+import java.net.URI;
+
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.fs.permission.FsAction;
+import org.apache.hadoop.fs.permission.FsPermission;
 import org.apache.hadoop.mapred.JobConf;
 
 import voldemort.VoldemortException;
@@ -37,6 +41,12 @@ import voldemort.store.readonly.mr.utils.VoldemortUtils;
 import voldemort.store.readonly.swapper.AdminStoreSwapper;
 import voldemort.store.readonly.swapper.FailedFetchStrategy;
 import voldemort.utils.logging.PrefixedLogger;
+import voldemort.utils.Props;
+
+import gobblin.runtime.api.JobExecutionResult;
+import gobblin.runtime.embedded.EmbeddedGobblin;
+import gobblin.runtime.embedded.EmbeddedGobblinDistcp;
+
 import azkaban.jobExecutor.AbstractJob;
 
 /*
@@ -57,6 +67,7 @@ public class VoldemortSwapJob extends AbstractJob {
     private final String dataDir;
     private final String clusterName;
     private final boolean buildPrimaryReplicasOnly;
+    private final Props props;
 
     // The following internal state mutates during run()
     private long pushVersion;
@@ -74,7 +85,8 @@ public class VoldemortSwapJob extends AbstractJob {
                             int maxNodeFailures,
                             List<FailedFetchStrategy> failedFetchStrategyList,
                             String clusterName,
-                            boolean buildPrimaryReplicasOnly) throws IOException {
+                            boolean buildPrimaryReplicasOnly,
+                            Props props) throws IOException {
         super(id, PrefixedLogger.getLogger(AdminStoreSwapper.class.getName(), clusterName));
         this.cluster = cluster;
         this.dataDir = dataDir;
@@ -89,6 +101,89 @@ public class VoldemortSwapJob extends AbstractJob {
         this.failedFetchStrategyList = failedFetchStrategyList;
         this.clusterName = clusterName;
         this.buildPrimaryReplicasOnly = buildPrimaryReplicasOnly;
+        this.props = props;
+    }
+
+    private void runDistcp(Path from, Path to) throws Exception {
+        info("     source: " + from);
+        info("destination: " + to);
+        EmbeddedGobblin embeddedGobblin = new EmbeddedGobblinDistcp(from, to).mrMode();
+
+        // Used for global throttling"
+        embeddedGobblin.distributeJar("lib/*");
+
+        for (Map.Entry<String, String> entry : this.props.entrySet()) {
+            if (entry.getKey() instanceof String && ((String) entry.getKey()).startsWith("distcpConf.")) {
+                String key = ((String) entry.getKey()).substring("distcpConf.".length());
+                embeddedGobblin.setConfiguration(key, entry.getValue().toString());
+            }
+        }
+        JobExecutionResult result =  embeddedGobblin.run();
+
+        if (!result.isSuccessful()) {
+            throw new RuntimeException("Distcp job failed!", result.getErrorCause());
+        }
+    }
+
+    private FileSystem getRootFS(String target) throws Exception {
+        URI rootURI = new URI(target.replaceAll("(?<=:[0-9]{1,5})/.*", ""));
+        FileSystem rootFS = FileSystem.get(rootURI, new JobConf());
+        return rootFS;
+    }
+
+    private void deleteDir(FileSystem fs, String target) throws Exception {
+        Path path = new Path(target.replaceAll(".*://.*?(?=/)", ""));
+
+        if (fs.exists(path)) {
+            fs.delete(path, true);
+            if (fs.exists(path)) {
+                warn("Could not delete temp directory " + path + " in CDN!");
+            } else {
+                info("Deleted " + path);
+            }
+        }
+    }
+
+    private void deleteDirOnExit(FileSystem fs, String target) throws Exception {
+        Path path = new Path(target.replaceAll(".*://.*?(?=/)", ""));
+
+        fs.deleteOnExit(path);  // Delete the directory even if an exception occurs
+        info(path + " is scheduled to be deleted on exit.");
+    }
+
+    private void addPermissionsToParents(FileSystem fs, String target) throws Exception {
+        String pathFromRoot = target.replaceAll(".*://.*?/", "");
+        String parent = "";
+
+        for (String seg: pathFromRoot.split("/")) {
+            parent = parent + "/" + seg;
+            Path parentPath = new Path(parent);
+            FsPermission perm = fs.getFileStatus(parentPath).getPermission();
+            if (!perm.getOtherAction().implies(FsAction.READ_EXECUTE)) {
+                fs.setPermission(parentPath, new FsPermission(perm.getUserAction(), perm.getGroupAction(), perm.getOtherAction().or(FsAction.READ_EXECUTE)));
+                info("for path " + parent + ", permissions changed:");
+                info("from: " + perm.toString());
+                info("  to: " + fs.getFileStatus(parentPath).getPermission().toString());
+            }
+        }
+    }
+
+    private String pickCDN() {
+        List<String> pushClusters = props.getList(VoldemortBuildAndPushJob.PUSH_CLUSTER);
+
+        if (!props.containsKey(VoldemortBuildAndPushJob.PUSH_CDN_CLUSTER)) {
+            return "";
+        }
+        List<String> cdnClusters = props.getList(VoldemortBuildAndPushJob.PUSH_CDN_CLUSTER);
+
+        if (pushClusters.size() != cdnClusters.size()) {
+            warn("Cluster sizes are different!");
+            warn("Will bypass CDN!");
+            return "";
+        }
+
+        int index = pushClusters.indexOf(clusterName);
+        return index == -1 ? "" : cdnClusters.get(index);
     }
 
     public void run() throws Exception {
@@ -98,6 +193,35 @@ public class VoldemortSwapJob extends AbstractJob {
         JobConf conf = new JobConf();
         Path dataPath = new Path(dataDir);
         String modifiedDataDir = dataPath.makeQualified(FileSystem.get(conf)).toString();
+        String cdnURL = pickCDN();
+
+        if (!cdnURL.isEmpty()) {
+            info("Using CDN: " + cdnURL);
+            info("####################################");
+            info("              Distcp");
+            info("####################################");
+            try {
+                String username = props.getString("env.USER", "unknownClient");  // TODO: Is this attribute always existing?
+                String pathPrefix = props.getString(VoldemortBuildAndPushJob.PUSH_CDN_PREFIX, "/tmp/VoldemortBnP/");
+                pathPrefix = pathPrefix.endsWith("/") ? pathPrefix : pathPrefix + "/";
+                String cdnDir = modifiedDataDir.replaceAll(".*://.*?(?=/)", cdnURL + pathPrefix + username);
+                FileSystem cdnRootFS = getRootFS(cdnDir);
+                Path from = new Path(modifiedDataDir);
+                Path to = new Path(cdnDir);
+
+                deleteDir(cdnRootFS, cdnDir);
+                runDistcp(from, to);
+                deleteDirOnExit(cdnRootFS, cdnDir);
+                addPermissionsToParents(cdnRootFS, cdnDir);
+                modifiedDataDir = cdnDir;
+            } catch (Exception e) {
+                warn("An exception occured during distcp: " + e.getMessage());
+                warn("Use original HDFS cluster: " + modifiedDataDir);
+            }
+            info("####################################");
+            info("          End of Distcp");
+            info("####################################");
+        }
 
         /*
          * Replace the default protocol and port with the one derived as above
